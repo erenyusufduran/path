2d1b04f8cbb924f2c73b2fe1b8d0afd0818609ea
erenyusufduran <erenyusuduran1905@gmail.com>
2023-02-13 11:32:08 +0300

    refactor the code

--- 07-js-algorithms/01-problem-solving/charCount.js
+++ 07-js-algorithms/01-problem-solving/charCount.js
@@ -8,13 +8,25 @@
  */
 function charCount(str) {
   const obj = new Object();
-  const newArr = str.toLowerCase().split("");
-  newArr.forEach((element) => {
-    if (/[a-z0-9]/.test(element)) {
-      obj[element] = ++obj[element] || 1;
+  for (let char of str) {
+    if (isAlphaNumeric(char)) {
+      char = char.toLowerCase();
+      obj[char] = ++obj[char] || 1;
     }
-  });
+  }
   console.log(obj);
 }
 
-charCount("ER  - EN Yasdasd");
+function isAlphaNumeric(char) {
+  const code = char.charCodeAt(0);
+  if (
+    !(code > 47 && code < 58) &&
+    !(code > 64 && code < 91) &&
+    !(code > 96 && code < 123)
+  ) {
+    return false;
+  }
+  return true;
+}
+
+charCount("ER  - EN Yasd etrsdg  as FLskd .. . asşdlas");

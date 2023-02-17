3028d947962dd95dc5507ba5fe1f8c58b3fad8ba
erenyusufduran <erenyusuduran1905@gmail.com>
2023-02-17 19:58:37 +0300

    queues

--- 07-js-algorithms/05-data-structures/01-stack-queues/01-queues/queue-linked-list.js
+++ 07-js-algorithms/05-data-structures/01-stack-queues/01-queues/queue-linked-list.js
@@ -1,31 +1,30 @@
 class Node {
-  constructor(value) {
-    this.value = value;
+  constructor(val) {
+    this.value = val;
     this.next = null;
   }
 }
 
-class Stack {
+class Queue {
   constructor() {
     this.first = null;
     this.last = null;
     this.size = 0;
   }
-  // adding to the beginning
-  push(val) {
-    const newNode = new Node(val);
+  // adding to the end
+  enqueue(val) {
+    const node = new Node(val);
     if (!this.first) {
-      this.first = newNode;
-      this.last = newNode;
+      this.first = node;
+      this.last = node;
     } else {
-      const temp = this.first;
-      this.first = newNode;
-      this.first.next = temp;
+      this.last.next = node;
+      this.last = node;
     }
     return ++this.size;
   }
   // removing from the beginning
-  pop() {
+  dequeue() {
     if (!this.first) return null;
     const temp = this.first;
     if (this.first === this.last) {
---
+++ 07-js-algorithms/05-data-structures/01-stack-queues/01-queues/queues.md
@@ -0,0 +1,31 @@
+# Queues
+
+A **FIFO** data structure.
+
+> ### **FIRST IN FIRST OUT**
+
+Queues exist everywhere! Think about the last time you waited in line...
+
+## **How do we use them in programming?**
+- Background Tasks
+- Uploading Resources
+- Printing / Task Processing
+
+>  ### There is more than one way of implementing a Queue.
+- **Array Implementation**
+    - Use push and shift methods
+    - Use unshift and pop methods
+- **Linked List Implementation**
+
+
+
+**Big O of Stacks**
+- Insertion - **O(1)**
+- Removal - **O(1)**
+- Searching - **O(n)**
+- Access - **O(n)**
+
+## Recap
+- Queues are **FIFO** data structure, all elements are first in first out.
+- Queues are useful for processing tasks and are foundational for more complex data structures.
+- Insertion and Removal can be done in O(1)

6881e7c9d3efef93669d4a1863b4bf1e978c8d04
erenyusufduran <erenyusuduran1905@gmail.com>
2023-02-22 16:19:20 +0300

    yargs builder, more advance

--- 01-nodejs/course/01-notes-app/app.js
+++ 01-nodejs/course/01-notes-app/app.js
@@ -7,8 +7,21 @@ const getNotes = require("./notes.js");
 yargs.command({
   command: "add",
   describe: "Add a new note",
-  handler: function () {
-    console.log("Adding a new note!");
+  builder: {
+    title: {
+      describe: "Note title",
+      demandOption: true,
+      type: "string",
+    },
+    body: {
+      describe: "Note body",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler: function (argv) {
+    console.log("Title:", argv.title);
+    console.log("Body: ", argv.body);
   },
 });
 
@@ -35,5 +48,7 @@ yargs.command({
     console.log("Reading a note");
   },
 });
+
+yargs.parse();
 
 console.log(yargs.argv);

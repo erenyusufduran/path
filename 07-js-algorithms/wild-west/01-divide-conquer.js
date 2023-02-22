d1111eec5cf8a5d09a8eea192a1b9147827a6366
erenyusufduran <erenyusuduran1905@gmail.com>
2023-02-22 09:01:51 +0300

    find rotated index

--- 07-js-algorithms/wild-west/01-divide-conquer.js
+++ 07-js-algorithms/wild-west/01-divide-conquer.js
@@ -22,3 +22,15 @@ function sortedFrequency(arr, num) {
   }
   return count ? count : -1;
 }
+
+// Write a function called findRotatedIndex which accepts a rotated array of
+// sorted numbers and an integer. The function should return the index of the integer
+// in the array. If the value is not found, return -1.
+function findRotatedIndex(arr, num) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === num) {
+      return i;
+    }
+  }
+  return -1;
+}

35262dcb0ffafcb6d3bd779d82152a60687343c6
erenyusufduran <erenyusuduran1905@gmail.com>
2023-02-23 22:42:59 +0300

    customizing http req

--- 01-nodejs/course/02-weather-app/app.js
+++ 01-nodejs/course/02-weather-app/app.js
@@ -1,9 +1,11 @@
 const request = require("request");
 require("dotenv").config();
 
-const url = `http://api.weatherstack.com/current?access_key=${process.env.ACCESS_KEY}&query=New%20York`;
+const url = `http://api.weatherstack.com/current?access_key=${process.env.ACCESS_KEY}&query=Kayseri`;
 
-request({ url }, (error, response) => {
-  const data = JSON.parse(response.body);
-  console.log(data.current);
+request({ url, json: true }, (error, response) => {
+  const res = response.body.current;
+  console.log(
+    `It is currently ${res.temperature} degrees out. There is a %${res.precip} chance of rain`
+  );
 });

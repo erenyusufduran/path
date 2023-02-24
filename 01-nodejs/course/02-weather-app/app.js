aa4909d96ff71e47fe96749e2a4f74517e44c395
erenyusufduran <erenyusuduran1905@gmail.com>
2023-02-24 09:53:11 +0300

    geocode api - mapbox

--- 01-nodejs/course/02-weather-app/app.js
+++ 01-nodejs/course/02-weather-app/app.js
@@ -9,3 +9,12 @@ request({ url, json: true }, (error, response) => {
     `It is currently ${res.temperature} degrees out. There is a %${res.precip} chance of rain`
   );
 });
+
+const geocodeURL =
+  "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiZXJlbi1kdXJhbiIsImEiOiJjbGVpNjUyb2EwMDc0NDVxc3owYTBpbnZ2In0.DNAkP2Fv4rxmDTShMM39yQ&limit=1";
+
+request({ url: geocodeURL, json: true }, (error, response) => {
+  const latitude = response.body.features[0].center[1];
+  const longitude = response.body.features[0].center[0];
+  console.log(latitude, longitude);
+});

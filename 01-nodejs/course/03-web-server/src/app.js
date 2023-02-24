e69599a2ff639d616ce1a99bf020df621294507a
erenyusufduran <erenyusuduran1905@gmail.com>
2023-02-24 12:29:04 +0300

    serving up static assets

--- 01-nodejs/course/03-web-server/src/app.js
+++ 01-nodejs/course/03-web-server/src/app.js
@@ -1,20 +1,10 @@
+const path = require("path");
 const express = require("express");
 
 const app = express();
 
-app.get("", (req, res) => {
-  res.send("Hello express!");
-});
-
-app.get("/help", (req, res) => {
-  res.send({
-    name: "eren",
-  });
-});
-
-app.get("/about", (req, res) => {
-  res.send("<h1>About</h1>");
-});
+const publicDirectoryPath = path.join(__dirname, "../public");
+app.use(express.static(publicDirectoryPath));
 
 app.get("/weather", (req, res) => {
   res.send({

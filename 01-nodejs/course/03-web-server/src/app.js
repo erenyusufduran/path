dab82d295dd2adec697c8f7b9750e8319aca1fb2
erenyusufduran <erenyusuduran1905@gmail.com>
2023-02-24 22:17:15 +0300

    404 pages

--- 01-nodejs/course/03-web-server/src/app.js
+++ 01-nodejs/course/03-web-server/src/app.js
@@ -45,6 +45,22 @@ app.get("/weather", (req, res) => {
   });
 });
 
+app.get("/help/*", (req, res) => {
+  res.render("404", {
+    title: "404",
+    name: "Eren Yusuf Duran",
+    errorMessage: "Help article not found!",
+  });
+});
+
+app.get("*", (req, res) => {
+  res.render("404", {
+    title: "404",
+    name: "Eren Yusuf Duran",
+    errorMessage: "Page not found!",
+  });
+});
+
 app.listen(3000, () => {
   console.log("Server is up on port 3000.");
 });

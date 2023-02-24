b910175a0d27fc54407236dd440b93bf2eb9e634
erenyusufduran <erenyusuduran1905@gmail.com>
2023-02-24 12:49:41 +0300

    dynamic pages with handlebars

--- 01-nodejs/course/03-web-server/src/app.js
+++ 01-nodejs/course/03-web-server/src/app.js
@@ -1,10 +1,32 @@
 const path = require("path");
 const express = require("express");
 
+const publicDirectoryPath = path.join(__dirname, "../public");
+
 const app = express();
 
-const publicDirectoryPath = path.join(__dirname, "../public");
+app.set("view engine", "hbs");
 app.use(express.static(publicDirectoryPath));
+
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "Weather App",
+    name: "Eren Yusuf Duran",
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About Page",
+  });
+});
+
+app.get("/help", (req, res) => {
+  res.render("help", {
+    title: "Help Page",
+    helpText: "This is some helpful text.",
+  });
+});
 
 app.get("/weather", (req, res) => {
   res.send({

3ad1355a014fa37f1cc3898a4171e02fc695a01b
erenyusufduran <erenyusuduran1905@gmail.com>
2023-02-27 01:06:15 +0300

    updating docs

--- 01-nodejs/course/04-task-manager/mongodb.js
+++ 01-nodejs/course/04-task-manager/mongodb.js
@@ -3,29 +3,41 @@ const { MongoClient, ObjectID } = require("mongodb");
 const connectionURL = "mongodb://localhost:27017/";
 const databaseName = "task-manager";
 
-MongoClient.connect(
-  connectionURL,
-  { useNewUrlParser: true, useUnifiedTopology: true },
-  (error, client) => {
-    if (error) {
-      return console.log("Ubanle to connect to database!");
-    }
-
-    const db = client.db(databaseName);
-    // db.collection("users").findOne(
-    //   { _id: new ObjectID("63fb86709bbeb24adc7f8fd8") },
-    //   (error, user) => {
-    //     if (error) {
-    //       return console.log("Unable to fetch");
-    //     }
-    //     console.log(user);
-    //   }
-    // );
-
-    db.collection("users")
-      .find({ age: 21 })
-      .toArray((error, users) => {
-        console.log(users);
-      });
+MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
+  if (error) {
+    return console.log("Ubanle to connect to database!");
   }
-);
+
+  const db = client.db(databaseName);
+
+  db.collection("users")
+    .updateOne(
+      {
+        _id: new ObjectID("63fb86709bbeb24adc7f8fd9"),
+      },
+      {
+        $set: {
+          name: "Ako",
+        },
+        $inc: {
+          age: 1,
+        },
+      }
+    )
+    .then((result) => console.log(result))
+    .catch((err) => console.log(err));
+
+  db.collection("tasks")
+    .updateMany(
+      {
+        completed: false,
+      },
+      {
+        $set: {
+          completed: true,
+        },
+      }
+    )
+    .then((result) => console.log(result))
+    .catch((err) => console.log(err));
+});

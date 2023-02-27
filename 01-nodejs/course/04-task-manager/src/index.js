30c02fd362d678da220e4c79cff1dbf5a7632556
erenyusufduran <erenyusuduran1905@gmail.com>
2023-02-27 11:15:03 +0300

    reading endpoints

--- 01-nodejs/course/04-task-manager/src/index.js
+++ 01-nodejs/course/04-task-manager/src/index.js
@@ -16,11 +16,42 @@ app.post("/users", (req, res) => {
     .catch((err) => res.status(400).send(err));
 });
 
+app.get("/users", (req, res) => {
+  User.find({})
+    .then((users) => res.status(200).send(users))
+    .catch((err) => res.status(400).send(err));
+});
+
+app.get("/users/:id", (req, res) => {
+  const { id } = req.params;
+  User.findById(id)
+    .then((user) => {
+      if (!user) return res.status(404).send();
+      res.status(200).send(user);
+    })
+    .catch((err) => res.status(400).send(err));
+});
+
 app.post("/tasks", (req, res) => {
   const task = new Task(req.body);
   task
     .save()
     .then((result) => res.status(201).send(result))
+    .catch((err) => res.status(400).send(err));
+});
+
+app.get("/tasks", (req, res) => {
+  Task.find({})
+    .then((tasks) => res.status(200).send(tasks))
+    .catch((err) => res.status(400).send(err));
+});
+
+app.get("/tasks/:id", (req, res) => {
+  const { id } = req.params;
+  Task.findById(id)
+    .then((task) => {
+      res.status(200).send(task);
+    })
     .catch((err) => res.status(400).send(err));
 });
 

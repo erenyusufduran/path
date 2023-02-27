d62309499aaee3445f9f94d250e61f6b517e876d
erenyusufduran <erenyusuduran1905@gmail.com>
2023-02-27 18:53:47 +0300

    accepting auth tokens with auth middleware

--- 01-nodejs/course/04-task-manager/src/routers/user.js
+++ 01-nodejs/course/04-task-manager/src/routers/user.js
@@ -1,5 +1,6 @@
 const { Router } = require("express");
 const User = require("../models/user");
+const auth = require("../middleware/auth");
 const router = new Router();
 
 router.post("/users", async (req, res) => {
@@ -23,13 +24,8 @@ router.post("/users/login", async (req, res) => {
   }
 });
 
-router.get("/users", async (req, res) => {
-  try {
-    const users = await User.find();
-    res.status(200).send(users);
-  } catch (error) {
-    res.status(500).send(error);
-  }
+router.get("/users/me", auth, async (req, res) => {
+  res.send(req.user);
 });
 
 router.get("/users/:id", async (req, res) => {

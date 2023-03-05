1affac60d40f3d14a21d1583498ac1ce38916b0e
erenyusufduran <erenyusuduran1905@gmail.com>
2023-03-05 18:10:11 +0300

    validating file uploads

--- 01-nodejs/course/04-task-manager/src/routers/user.js
+++ 01-nodejs/course/04-task-manager/src/routers/user.js
@@ -87,6 +87,15 @@ router.delete("/users/me", auth, async (req, res) => {
 
 const upload = multer({
   dest: "avatars",
+  limits: {
+    fileSize: 1000000,
+  },
+  fileFilter(req, file, cb) {
+    if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+      return cb(new Error("Please upload an image."));
+    }
+    cb(undefined, true);
+  },
 });
 
 router.post("/users/me/avatar", upload.single("avatar"), async (req, res) => {

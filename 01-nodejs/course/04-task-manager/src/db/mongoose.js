b9084ccfb751623270a6f7601e3d43b784b1c8b9
erenyusufduran <erenyusuduran1905@gmail.com>
2023-03-07 13:42:07 +0300

    testing with authentication

--- 01-nodejs/course/04-task-manager/src/db/mongoose.js
+++ 01-nodejs/course/04-task-manager/src/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/task-manager-api-test", {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useCreateIndex: true,
   useUnifiedTopology: true,

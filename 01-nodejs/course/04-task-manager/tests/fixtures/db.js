28419515605382bfd7a881ea13ba1b7d6039c7d4
erenyusufduran <erenyusuduran1905@gmail.com>
2023-03-07 15:21:47 +0300

    setup task test suire

---
+++ 01-nodejs/course/04-task-manager/tests/fixtures/db.js
@@ -0,0 +1,23 @@
+const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
+const User = require("../../src/models/user");
+
+const userOneId = new mongoose.Types.ObjectId();
+const userOne = {
+  _id: userOneId,
+  name: "Mike",
+  email: "mike@example.com",
+  password: "5468what??",
+  tokens: [
+    {
+      token: jwt.sign({ _id: userOneId }, process.env.JWT_STRING),
+    },
+  ],
+};
+
+const setupDatabase = async () => {
+  await User.deleteMany();
+  await new User(userOne).save();
+};
+
+module.exports = { userOneId, userOne, setupDatabase };

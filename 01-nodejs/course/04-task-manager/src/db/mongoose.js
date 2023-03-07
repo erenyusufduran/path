4fba75cc4f23a0517baba9f18da25019a5963d49
erenyusufduran <erenyusuduran1905@gmail.com>
2023-03-07 09:49:37 +0300

    testing async code

--- 01-nodejs/course/04-task-manager/src/db/mongoose.js
+++ 01-nodejs/course/04-task-manager/src/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGO_URI, {
+mongoose.connect("mongodb+srv://eren123:eren123@taskmanager.jtjk7.mongodb.net/?retryWrites=true&w=majority", {
   useNewUrlParser: true,
   useCreateIndex: true,
   useUnifiedTopology: true,

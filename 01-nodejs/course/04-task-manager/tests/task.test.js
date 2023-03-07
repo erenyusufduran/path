a17b985abf25b17d95a9236d01c46cd567340c52
erenyusufduran <erenyusuduran1905@gmail.com>
2023-03-07 15:50:48 +0300

    extra tests for tasks

--- 01-nodejs/course/04-task-manager/tests/task.test.js
+++ 01-nodejs/course/04-task-manager/tests/task.test.js
@@ -16,6 +16,14 @@ test("Should create task for user", async () => {
   expect(task.completed).toBe(false);
 });
 
+test("Should not create task with invalid description/completed", async () => {
+  await request(app)
+    .post("/tasks")
+    .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+    .send({ new: true })
+    .expect(500);
+});
+
 test("Should fetch user tasks", async () => {
   const res = await request(app)
     .get("/tasks")
@@ -30,6 +38,38 @@ test("Should fetch user tasks", async () => {
     .send()
     .expect(200);
   expect(secondRes.body.length).toEqual(1);
+});
+
+test("Should fetch only completed/incompleted tasks", async () => {
+  let res = await request(app)
+    .get("/tasks?completed=true")
+    .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+    .send()
+    .expect(200);
+  expect(res.body.length).toBe(1);
+
+  res = await request(app)
+    .get("/tasks?completed=false")
+    .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+    .send()
+    .expect(200);
+  expect(res.body.length).toBe(1);
+});
+
+test("Should fetch user task by id", async () => {
+  await request(app)
+    .get(`/tasks/${taskOne._id}`)
+    .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+    .send()
+    .expect(200);
+});
+
+test("Should delete user task", async () => {
+  await request(app)
+    .delete(`/tasks/${taskOne._id}`)
+    .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+    .send()
+    .expect(200);
 });
 
 test("Should not delete other users tasks", async () => {

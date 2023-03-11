db1a3e1ab845d3dec0aa4e1c836e4a1a3bf77350
erenyusufduran <erenyusuduran1905@gmail.com>
2023-03-12 01:03:01 +0300

    socket events

--- 01-nodejs/course/05-chat-app/src/index.js
+++ 01-nodejs/course/05-chat-app/src/index.js
@@ -3,17 +3,26 @@ const http = require("http");
 const express = require("express");
 const socketio = require("socket.io");
 
+const PORT = process.env.PORT || 3000;
+
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
-const PORT = process.env.PORT || 3000;
 const publicDirectoryPath = path.join(__dirname, "../public");
 
 app.use(express.static(publicDirectoryPath));
 
-io.on("connection", () => {
+let count = 0;
+
+io.on("connection", (socket) => {
   console.log("New Websocket Connection.");
+  socket.emit("countUpdated", count);
+  socket.on("increment", () => {
+    count++;
+    // socket.emit("countUpdated", count); --> this will emit for one client so we use below
+    io.emit("countUpdated", count); // emit for everyone
+  });
 });
 
 server.listen(PORT, () => {

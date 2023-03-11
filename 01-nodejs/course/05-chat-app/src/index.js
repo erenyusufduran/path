1d2a9d4940e3b57ccb94fc418dd2d95f5d08bcd2
erenyusufduran <erenyusuduran1905@gmail.com>
2023-03-12 01:19:13 +0300

    broadcasting events, disconect

--- 01-nodejs/course/05-chat-app/src/index.js
+++ 01-nodejs/course/05-chat-app/src/index.js
@@ -17,9 +17,14 @@ io.on("connection", (socket) => {
   console.log("New Websocket Connection.");
 
   socket.emit("message", "Welcome!");
+  socket.broadcast.emit("message", "A new user has joined"); // emit for everybody except particular connection.
 
   socket.on("sendMessage", (message) => {
     io.emit("message", message);
+  });
+
+  socket.on("disconnect", () => {
+    io.emit("message", "A user has left!");
   });
 });
 

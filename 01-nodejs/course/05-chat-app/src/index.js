9196288e250d1c82a5ade45e20b958e0960f062f
erenyusufduran <erenyusuduran1905@gmail.com>
2023-03-12 01:14:36 +0300

    more examples about events

--- 01-nodejs/course/05-chat-app/src/index.js
+++ 01-nodejs/course/05-chat-app/src/index.js
@@ -13,15 +13,13 @@ const publicDirectoryPath = path.join(__dirname, "../public");
 
 app.use(express.static(publicDirectoryPath));
 
-let count = 0;
-
 io.on("connection", (socket) => {
   console.log("New Websocket Connection.");
-  socket.emit("countUpdated", count);
-  socket.on("increment", () => {
-    count++;
-    // socket.emit("countUpdated", count); --> this will emit for one client so we use below
-    io.emit("countUpdated", count); // emit for everyone
+
+  socket.emit("message", "Welcome!");
+
+  socket.on("sendMessage", (message) => {
+    io.emit("message", message);
   });
 });
 

03b80974f2edf8c61973c567c427c8870ad1a58d
erenyusufduran <eren.duran@colins.com>
2023-03-18 11:55:01 +0300

    leetcode, single numbers

--- 10-leetcode/easy/single-number.js
+++ 10-leetcode/easy/single-number.js
@@ -17,3 +17,25 @@ var singleNumber = function (nums) {
     }
   }
 };
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var singleNumbers = function (nums) {
+  const obj = new Object();
+  const arr = [];
+  nums.forEach((num) => {
+    if (!obj[num]) {
+      obj[num] = 1;
+    } else {
+      obj[num] += 1;
+    }
+  });
+  for (let el in obj) {
+    if (obj[el] === 1) {
+      arr.push(el);
+    }
+  }
+  return arr;
+};

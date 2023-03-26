40392c70b5ac8339eb56e692a0720e6a7696063b
erenyusufduran <eren.duran@colins.com>
2023-03-26 15:49:13 +0300

    leetcode pivot index

---
+++ 07-js-algorithms/09-leetcode/easy/pivot.index.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var pivotIndex = function (nums) {
+  let totalSum = 0;
+  let leftSum = 0;
+  nums.forEach((num) => (totalSum += num));
+  for (let i = 0; i < nums.length; i++) {
+    if (totalSum - leftSum - nums[i] === leftSum) {
+      return i;
+    }
+    leftSum += nums[i];
+  }
+  return -1;
+};

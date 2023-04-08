ebf000a86013afdd5fe6c125653ccddd33c5bf10
erenyusufduran <eren.duran@colins.com>
2023-04-08 03:57:34 +0300

    when to use annotations

--- 10-typescript/01-features/annotations/variables.ts
+++ 10-typescript/01-features/annotations/variables.ts
@@ -3,9 +3,10 @@
  * Type inference -> Typescript tries to figure out what type of value a variable refers to
  */
 
+// If declaration and initialization are on the same line, ts can understand what type is it.ş
 let apples = 5;
-let speed = "fast";
-let hasName = true;
+let speed: string = "fast";
+let hasName: boolean = true;
 
 let nothingMuch = null;
 let nothing = undefined;
@@ -33,3 +34,27 @@ let point: { x: number; y: number } = {
 const logNumber: (i: number) => void = (i: number) => {
   console.log(i);
 };
+
+// When to use annotations
+// 1) Function that returns the 'any' type
+const json = '{"x": 10, "y":20}';
+const coordinates: { x: number; y: number } = JSON.parse(json);
+console.log(coordinates);
+
+// 2) When we declare a variable on one line and initialize it later
+let words = ["red", "green", "blue"];
+let foundWord: boolean;
+for (let i = 0; i < words.length; i++) {
+  if (words[i] === "green") {
+    foundWord = true;
+  }
+}
+
+// 3) Variable whose type cannot be inferred correctly
+let numbers = [-10, -1, 12];
+let numberAboveZero: boolean | number = false;
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] > 0) {
+    numberAboveZero = numbers[i];
+  }
+}

460f9405a2b047a10d533179df95200144e365ad
erenyusufduran <eren.duran@colins.com>
2023-04-09 17:15:20 +0300

    constructor and pub privs in classes

--- 10-typescript/01-features/annotations/classes.ts
+++ 10-typescript/01-features/annotations/classes.ts
@@ -2,13 +2,16 @@
 // Blueprint to create an object with some fields and methods to represent a 'thing'
 
 class Vehicle {
+  constructor(public color: string) {}
   protected honk(): void {
     console.log("Beep");
   }
 }
 
 class Car extends Vehicle {
-  // overwrite
+  constructor(public wheels: number, color: string) {
+    super(color);
+  }
   private drive(): void {
     console.log("Vroom");
   }
@@ -18,5 +21,6 @@ class Car extends Vehicle {
   }
 }
 
-const car = new Car();
+const car = new Car(4, "red");
+console.log(car.color);
 car.startDrivingProcess();

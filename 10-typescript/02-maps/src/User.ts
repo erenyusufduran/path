b4dc05072426f81319e844f147d27b7de958f46e
erenyusufduran <eren.duran@colins.com>
2023-04-09 17:46:03 +0300

    user class

--- 10-typescript/02-maps/src/User.ts
+++ 10-typescript/02-maps/src/User.ts
@@ -1,1 +1,18 @@
-class User {}
+import { faker } from "@faker-js/faker";
+
+export class User {
+  name: string;
+  location: {
+    lat: number;
+    lng: number;
+  };
+
+  constructor() {
+    this.name = faker.name.firstName();
+    console.log(this.location);
+    this.location = {
+      lat: parseFloat(faker.address.latitude()),
+      lng: parseFloat(faker.address.longitude()),
+    };
+  }
+}

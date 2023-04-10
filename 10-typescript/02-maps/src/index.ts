d25a7bef38939f4f3b285fcb38bb8168482dca11
erenyusufduran <eren.duran@colins.com>
2023-04-10 14:13:15 +0300

    google map integration

--- 10-typescript/02-maps/src/index.ts
+++ 10-typescript/02-maps/src/index.ts
@@ -1,4 +1,10 @@
+/// <reference types="@types/google.maps" />
 import { User } from "./User";
+import { Company } from "./Company";
 
 const user = new User();
 console.log(user);
+
+console.log(new Company());
+
+google;

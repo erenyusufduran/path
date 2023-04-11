dfa5a7081ec6590384e9722a6e388fc82c592277
erenyusufduran <eren.duran@colins.com>
2023-04-11 10:26:51 +0300

    showing popup and implements interfaces to classes

--- 10-typescript/02-maps/src/CustomMap.ts
+++ 10-typescript/02-maps/src/CustomMap.ts
@@ -1,13 +1,12 @@
 /// <reference types="@types/google.maps" />
-import { User } from "./User";
-import { Company } from "./Company";
 
 // Instructions to every other class on how they can be argument to 'addMarker'
-interface Mappable {
+export interface Mappable {
   location: {
     lat: number;
     lng: number;
   };
+  markerContent(): string;
 }
 
 export class CustomMap {
@@ -25,13 +24,18 @@ export class CustomMap {
   }
 
   addMarker(mappable: Mappable): void {
-    const { lat, lng } = mappable.location;
-    new google.maps.Marker({
+    const marker = new google.maps.Marker({
       map: this.googleMap,
       position: {
         lat: mappable.location.lat,
         lng: mappable.location.lng,
       },
+    });
+    marker.addListener("click", () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: mappable.markerContent(),
+      });
+      infoWindow.open(this.googleMap, marker);
     });
   }
 }

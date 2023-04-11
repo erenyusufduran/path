8ad7843263f4840c8c1bb7fd7d2e5eaaf0b209eb
erenyusufduran <eren.duran@colins.com>
2023-04-11 10:02:50 +0300

    addMarker with interfaces

--- 10-typescript/02-maps/src/CustomMap.ts
+++ 10-typescript/02-maps/src/CustomMap.ts
@@ -1,4 +1,14 @@
 /// <reference types="@types/google.maps" />
+import { User } from "./User";
+import { Company } from "./Company";
+
+// Instructions to every other class on how they can be argument to 'addMarker'
+interface Mappable {
+  location: {
+    lat: number;
+    lng: number;
+  };
+}
 
 export class CustomMap {
   private googleMap: google.maps.Map;
@@ -10,6 +20,17 @@ export class CustomMap {
       center: {
         lat: 0,
         lng: 0,
+      },
+    });
+  }
+
+  addMarker(mappable: Mappable): void {
+    const { lat, lng } = mappable.location;
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: mappable.location.lat,
+        lng: mappable.location.lng,
       },
     });
   }

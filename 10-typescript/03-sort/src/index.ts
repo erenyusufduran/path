c45b19b0d1ab4c01bdb75c164219bb77a170ea8e
erenyusufduran <eren.duran@colins.com>
2023-04-13 10:16:00 +0300

    sorting strings

--- 10-typescript/03-sort/src/index.ts
+++ 10-typescript/03-sort/src/index.ts
@@ -1,7 +1,15 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
+import { CharactersCollection } from "./CharactersCollection";
 
 const numbersCollection = new NumbersCollection([10, 3, -5, 2, -1, 0]);
+const charactersCollection = new CharactersCollection("Xazayb");
+
 const sorter = new Sorter(numbersCollection);
+const stringSorter = new Sorter(charactersCollection);
+
 sorter.sort();
 console.log(numbersCollection.data);
+
+stringSorter.sort();
+console.log(charactersCollection.data);

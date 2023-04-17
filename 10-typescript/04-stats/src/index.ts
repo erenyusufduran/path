9072f92c5c9070fb9f77a6bc4b3533570b396e58
erenyusufduran <eren.duran@colins.com>
2023-04-17 14:15:33 +0300

    html report

--- 10-typescript/04-stats/src/index.ts
+++ 10-typescript/04-stats/src/index.ts
@@ -1,6 +1,16 @@
 import { MatchReader } from "./MatchReader";
 import { CsvFileReader } from "./CsvFileReader";
+import { ConsoleReport } from "./reportTargets/ConsoleReport";
+import { WinsAnalysis } from "./analyzers/WinsAnalysis";
+import { Summary } from "./Summary";
+import { HtmlReport } from "./reportTargets/HtmlReport";
 
 const csvFileReader = new CsvFileReader("football.csv");
 const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
+
+const summary = new Summary(new WinsAnalysis("Man United"), new ConsoleReport());
+summary.buildAndPrintReport(matchReader.matches);
+
+const _summary = new Summary(new WinsAnalysis("Man United"), new HtmlReport());
+_summary.buildAndPrintReport(matchReader.matches);

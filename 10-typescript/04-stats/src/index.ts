17eec09718c0618995108828aba80bfc49a15d1e
erenyusufduran <eren.duran@colins.com>
2023-04-17 14:17:17 +0300

    static methods

--- 10-typescript/04-stats/src/index.ts
+++ 10-typescript/04-stats/src/index.ts
@@ -1,16 +1,14 @@
 import { MatchReader } from "./MatchReader";
-import { CsvFileReader } from "./CsvFileReader";
-import { ConsoleReport } from "./reportTargets/ConsoleReport";
-import { WinsAnalysis } from "./analyzers/WinsAnalysis";
 import { Summary } from "./Summary";
-import { HtmlReport } from "./reportTargets/HtmlReport";
 
-const csvFileReader = new CsvFileReader("football.csv");
-const matchReader = new MatchReader(csvFileReader);
+const matchReader = MatchReader.fromCsv("football.csv");
 matchReader.load();
 
-const summary = new Summary(new WinsAnalysis("Man United"), new ConsoleReport());
-summary.buildAndPrintReport(matchReader.matches);
+// const summary = new Summary(new WinsAnalysis("Man United"), new ConsoleReport());
+// summary.buildAndPrintReport(matchReader.matches);
 
-const _summary = new Summary(new WinsAnalysis("Man United"), new HtmlReport());
-_summary.buildAndPrintReport(matchReader.matches);
+// const _summary = new Summary(new WinsAnalysis("Man United"), new HtmlReport());
+// _summary.buildAndPrintReport(matchReader.matches);
+
+const summaryWithStaticMethods = Summary.winsAnalysisWithHtmlReport("Man United");
+summaryWithStaticMethods.buildAndPrintReport(matchReader.matches);

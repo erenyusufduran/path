feb8f7e85a39785caaf7f1b16282f4afafa06520
erenyusufduran <eren.duran@colins.com>
2023-04-18 11:44:41 +0300

    triggering events

--- 10-typescript/05-web/src/index.ts
+++ 10-typescript/05-web/src/index.ts
@@ -2,8 +2,16 @@ import { User } from "./models/User";
 
 const user = new User({ name: "Eren", age: 21 });
 
-user.on("change", () => {});
-user.on("change", () => {});
-user.on("ern", () => {});
+user.on("change", () => {
+  console.log("Change #1");
+});
+user.on("change", () => {
+  console.log("Change #2");
+});
+user.on("save", () => {
+  console.log("Save was triggered");
+});
+
+user.trigger("save");
 
 console.log(user);

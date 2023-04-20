d4a0861bab0a1c147b0463e49738e734ed90a631
erenyusufduran <eren.duran@colins.com>
2023-04-20 19:49:10 +0300

    adding model props, random age and re-rendering on model change

--- 10-typescript/05-web/src/index.ts
+++ 10-typescript/05-web/src/index.ts
@@ -1,4 +1,6 @@
+import { User } from "./models/User";
 import { UserForm } from "./views/UserForm";
 
-const userForm = new UserForm(document.getElementById("root") as Element);
+const user = User.buildUser({ name: "Name", age: 20 });
+const userForm = new UserForm(document.getElementById("root") as Element, user);
 userForm.render();
--- 10-typescript/05-web/src/views/UserForm.ts
+++ 10-typescript/05-web/src/views/UserForm.ts
@@ -1,27 +1,36 @@
+import { User } from "../models/User";
+
 export class UserForm {
-  constructor(public parent: Element) {}
+  constructor(public parent: Element, public model: User) {
+    this.bindModel();
+  }
+
+  bindModel(): void {
+    this.model.on("change", () => {
+      this.render();
+    });
+  }
 
   eventsMap(): { [key: string]: () => void } {
     return {
-      "click:button": this.onButtonClick,
-      "mouseenter:h1": this.onHeaderHover,
+      "click:#set-age": this.onSetAgeClick,
     };
   }
 
-  onHeaderHover(): void {
-    console.log("H1 was hovered");
-  }
-
-  onButtonClick(): void {
-    console.log("Hi there");
-  }
+  // normal func issue, do it with arrow funcs.
+  onSetAgeClick = (): void => {
+    this.model.setRandomAge();
+  };
 
   template(): string {
     return `
       <div>
         <h1>User Form</h1>
+        <div>User Name: ${this.model.get("name")}</div>
+        <div>User Age: ${this.model.get("age")}</div>
         <input />
         <button>Click Me</button>
+        <button id="set-age">Set Random Age</button>
       </div>
     `;
   }
@@ -37,6 +46,7 @@ export class UserForm {
   }
 
   render(): void {
+    this.parent.innerHTML = "";
     const templateElement = document.createElement("template");
     templateElement.innerHTML = this.template();
 

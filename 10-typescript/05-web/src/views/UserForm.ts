b53f28bf5873c0d683529c3ff56b7890e7320766
erenyusufduran <eren.duran@colins.com>
2023-04-20 20:57:34 +0300

    reading input text and strict null checks

--- 10-typescript/05-web/src/views/UserForm.ts
+++ 10-typescript/05-web/src/views/UserForm.ts
@@ -14,8 +14,19 @@ export class UserForm {
   eventsMap(): { [key: string]: () => void } {
     return {
       "click:#set-age": this.onSetAgeClick,
+      "click:#set-name": this.onSetNameClick,
     };
   }
+
+  onSetNameClick = (): void => {
+    const input = this.parent.querySelector("input");
+    if (input) {
+      const name = input.value;
+      this.model.set({ name });
+    } else {
+      throw new Error("There is no input element");
+    }
+  };
 
   // normal func issue, do it with arrow funcs.
   onSetAgeClick = (): void => {
@@ -29,7 +40,7 @@ export class UserForm {
         <div>User Name: ${this.model.get("name")}</div>
         <div>User Age: ${this.model.get("age")}</div>
         <input />
-        <button>Click Me</button>
+        <button id="set-name">Change Name</button>
         <button id="set-age">Set Random Age</button>
       </div>
     `;

b34410aac7f66f5d01642c12d1d9957009fa278d
erenyusufduran <eren.duran@colins.com>
2023-04-24 11:14:27 +0300

    userEdit and userShow, nesting with regions and mapping it

--- 10-typescript/05-web/src/views/View.ts
+++ 10-typescript/05-web/src/views/View.ts
@@ -1,11 +1,20 @@
 import { HasId, Model } from "../models/Model";
 
 export abstract class View<T extends Model<K>, K extends HasId> {
+  regions: { [key: string]: Element } = {};
+
   constructor(public parent: Element, public model: T) {
     this.bindModel();
   }
 
-  abstract eventsMap(): { [key: string]: () => void };
+  eventsMap(): { [key: string]: () => void } {
+    return {};
+  }
+
+  regionsMap(): { [key: string]: string } {
+    return {};
+  }
+
   abstract template(): string;
 
   bindModel(): void {
@@ -24,12 +33,29 @@ export abstract class View<T extends Model<K>, K extends HasId> {
     }
   }
 
+  mapRegions(fragment: DocumentFragment): void {
+    const regionsMap = this.regionsMap();
+    for (let key in regionsMap) {
+      const selector = regionsMap[key];
+      const element = fragment.querySelector(selector);
+      if (element) {
+        this.regions[key] = element;
+      }
+    }
+  }
+
+  onRender(): void {}
+
   render(): void {
     this.parent.innerHTML = "";
     const templateElement = document.createElement("template");
     templateElement.innerHTML = this.template();
 
     this.bindEvents(templateElement.content);
+    this.mapRegions(templateElement.content);
+
+    this.onRender();
+
     this.parent.append(templateElement.content);
   }
 }

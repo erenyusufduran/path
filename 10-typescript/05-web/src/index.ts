701efa97afd33c84292e6876b905ddde05196f0a
erenyusufduran <eren.duran@colins.com>
2023-04-24 11:43:59 +0300

    Collection View implementation

--- 10-typescript/05-web/src/index.ts
+++ 10-typescript/05-web/src/index.ts
@@ -1,12 +1,16 @@
-import { User } from "./models/User";
-import { UserEdit } from "./views/UserEdit";
+import { UserList } from "./views/UserList";
+import { Collection } from "./models/Collection";
+import { UserProps, User } from "./models/User";
 
-const user = User.buildUser({ name: "Name", age: 20 });
-const root = document.getElementById("root");
-if (root) {
-  const userEdit = new UserEdit(root, user);
-  userEdit.render();
-  console.log(userEdit);
-} else {
-  throw new Error("Root element not found.");
-}
+const users = new Collection("http://localhost:3000/users", (json: UserProps) => {
+  return User.buildUser(json);
+});
+
+users.on("change", () => {
+  const root = document.getElementById("root");
+  if (root) {
+    new UserList(root, users).render();
+  }
+});
+
+users.fetch();

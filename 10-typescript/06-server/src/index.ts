c90423288083cc7066d6d06b47e663dd51c4ac10
erenyusufduran <eren.duran@colins.com>
2023-04-25 09:28:59 +0300

    express basics with ts

--- 10-typescript/06-server/src/index.ts
+++ 10-typescript/06-server/src/index.ts
@@ -1,1 +1,17 @@
-console.log("hi there");
+import express, { Request, Response } from "express";
+import { router } from "./routes/loginRoutes";
+import bodyParser from "body-parser";
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/login", router);
+
+app.get("/", (req: Request, res: Response) => {
+  res.send(`
+    <div>
+      <h1>Hi There!</h1>
+    </div>
+  `);
+});
+
+app.listen(3000, () => console.log("Server is listening on port 3000"));

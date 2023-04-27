a594325ee805f044e7853a16609baddb456301cb
erenyusufduran <eren.duran@colins.com>
2023-04-27 14:27:00 +0300

    closed method sets with enums

--- 10-typescript/06-server/src/controllers/decorators/routes.ts
+++ 10-typescript/06-server/src/controllers/decorators/routes.ts
@@ -1,4 +1,5 @@
 import "reflect-metadata";
+import { Methods } from "./Methods";
 
 function routeBinder(method: string) {
   return function (path: string) {
@@ -9,5 +10,8 @@ function routeBinder(method: string) {
   };
 }
 
-export const get = routeBinder("get");
-export const post = routeBinder("post");
+export const get = routeBinder(Methods.get);
+export const post = routeBinder(Methods.post);
+export const put = routeBinder(Methods.put);
+export const patch = routeBinder(Methods.patch);
+export const del = routeBinder(Methods.del);

0ad673cb3db433b2bc7fe334a92b49136507c772
erenyusufduran <eren.duran@colins.com>
2023-04-27 14:14:28 +0300

    route class

--- 10-typescript/06-server/src/index.ts
+++ 10-typescript/06-server/src/index.ts
@@ -3,13 +3,13 @@ import { router } from "./routes/loginRoutes";
 import bodyParser from "body-parser";
 import cookieSession from "cookie-session";
 
-import { router as routerController } from "./controllers/decorators/controller";
+import { AppRouter } from "./AppRouter";
 import "./controllers/LoginController";
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieSession({ keys: ["eren"] }));
 app.use(router);
-app.use(routerController);
+app.use(AppRouter.getInstance());
 
 app.listen(3000, () => console.log("Server is listening on port 3000"));

dfa4e35d17d49bbefa112ee39cb8c61ba92fc0ed
erenyusufduran <eren.duran@colins.com>
2023-05-03 11:34:37 +0300

    validating store structure

--- 10-typescript/07-rrts/src/reducers/index.ts
+++ 10-typescript/07-rrts/src/reducers/index.ts
@@ -1,5 +1,10 @@
 import { combineReducers } from "redux";
+import { todosReducer } from "./todos";
+import { Todo } from "../actions";
+export interface StoreState {
+  todos: Todo[];
+}
 
-export const reducers = combineReducers({
-  counter: () => 1,
+export const reducers = combineReducers<StoreState>({
+  todos: todosReducer,
 });

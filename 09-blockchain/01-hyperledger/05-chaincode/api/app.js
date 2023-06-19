8f73d79c45ddfea003e789e87aef73a76c4ef0cb
erenyusufduran <eren.duran@colins.com>
2023-06-19 11:29:11 +0300

    chaincode api with fabric

---
+++ 09-blockchain/01-hyperledger/05-chaincode/api/app.js
@@ -0,0 +1,14 @@
+"use strict";
+const log4js = require("log4js");
+const logger = log4js.getLogger("BasicNetwork");
+const bodyParser = require("body-parser");
+const http = require("http");
+const util = require("util");
+const express = require("express");
+const expressJWT = require("express-jwt");
+const jwt = require("jsonwebtoken");
+const bearerToken = require("express-bearer-token");
+const cors = require("cors");
+const constants = require("./config/constants.json");
+
+const app = express();

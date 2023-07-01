fd5921ef4f78f71a1d8b6d21177dcdbe96e140a4
erenyusufduran <eren.duran@colins.com>
2023-07-01 17:33:52 +0300

    invoke

--- 09-blockchain/01-hyperledger/03-youtube/FabricNetwork/api/app/invoke.js
+++ 09-blockchain/01-hyperledger/03-youtube/FabricNetwork/api/app/invoke.js
@@ -1,9 +1,12 @@
 const { Gateway, Wallets } = require("fabric-network");
 const helper = require("./helper");
+const { blockListener, contractListener } = require("./listeners");
 
-const query = async (channelName, chaincodeName, args, fcn, username, orgName) => {
+const invokeTransaction = async (channelName, chaincodeName, fcn, args, username, orgName, transientData = []) => {
   try {
     const ccp = await helper.getCCP(orgName);
+    console.log("==================", channelName, chaincodeName, fcn, args, username, org_name);
+
     const walletPath = await helper.getWalletPath(orgName);
     const wallet = await Wallets.newFileSystemWallet(walletPath);
     console.log(`Wallet path: ${walletPath}`);
@@ -11,44 +14,37 @@ const query = async (channelName, chaincodeName, args, fcn, username, orgName) =
     let identity = await wallet.get(username);
     if (!identity) {
       console.log(`An identity for the user ${username} does not exist in the wallet, so registering user`);
-      await helper.getRegisteredUser(username, orgName, true);
+      await helper.getRegisteredUser(username, org_name, true);
       identity = await wallet.get(username);
       console.log("Run the registerUser.js application before retrying");
       return;
     }
 
-    // Create a new gateway for connecting to our peer node.
     const gateway = new Gateway();
-    await gateway.connect(ccp, {
-      wallet,
-      identity: username,
-      discovery: { enabled: true, asLocalhost: true },
-    });
+    await gateway.connect(ccp, { wallet, identity: username, discovery: { enabled: true, asLocalhost: true } });
     const network = await gateway.getNetwork(channelName);
     const contract = network.getContract(chaincodeName);
+
+    // await contract.addContractListener(contractListener)
+    // await network.addBlockListener(blockListener)
+
     let result;
+
     switch (fcn) {
-      case "GetDocumentUsingCarContract":
-        console.log("=============");
-        result = await contract.evaluateTransaction("SmartContract:" + fcn, args[0]);
-        break;
-      case "GetHistoryForAsset":
-      case "GetCarById":
-        console.log("=============");
-        result = await contract.evaluateTransaction(fcn, args[0]);
+      case "createCar":
+        result = await contract.submitTransaction(fcn, args[0]);
+        result = { txid: result.toString() };
         break;
       default:
         break;
     }
-    console.log(result);
-    console.log(`Transaction has been evaluated, result is: ${result.toString()}`);
 
-    result = JSON.parse(result.toString());
+    gateway.disconnect();
     return result;
   } catch (error) {
-    console.error(`Failed to evaluate transaction: ${error}`);
+    console.log(`Getting error: ${error}`);
     return error.message;
   }
 };
 
-exports.query = query;
+exports.invokeTransaction = invokeTransaction;

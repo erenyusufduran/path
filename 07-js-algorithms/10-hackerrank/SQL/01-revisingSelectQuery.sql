8f263ecc61a133f05d3ca0e39796f248997f4b2c
erenyusufduran <eren.duran@colins.com>
2023-07-20 11:17:45 +0300

    basic select queries

---
+++ 07-js-algorithms/10-hackerrank/SQL/01-revisingSelectQuery.sql
@@ -0,0 +1,23 @@
+-- Query all columns for all American cities IN the CITY TABLE WITH populations larger than 100000.The CountryCode for America is USA.
+SELECT * FROM CITY WHERE POPULATION > 100000 AND CountryCode = 'USA';
+    
+-- Query the NAME field for all American cities IN the CITY TABLE WITH populations larger than 120000.The CountryCode for America is USA.
+SELECT NAME FROM CITY WHERE POPULATION > 120000 AND CountryCode = 'USA';
+
+--Query all columns (attributes) for every row in the CITY table.
+SELECT * FROM CITY;
+
+--Query all columns for a city in CITY with the ID 1661.
+SELECT * FROM CITY WHERE ID = 1661;
+
+--Query all attributes of every Japanese city in the CITY table. The COUNTRYCODE for Japan is JPN.
+SELECT * FROM CITY WHERE CountryCode = 'JPN';
+
+--Query the names of all the Japanese cities in the CITY table. The COUNTRYCODE for Japan is JPN.
+SELECT NAME FROM CITY WHERE CountryCode = 'JPN';
+
+--Query a list of CITY and STATE from the STATION table.
+SELECT CITY, STATE FROM STATION;
+
+--Query a list of CITY names from STATION for cities that have an even ID number. Print the results in any order, but exclude duplicates from the answer.
+SELECT DISTINCT CITY FROM STATION WHERE MOD(ID, 2) = 0 ORDER BY CITY ASC;

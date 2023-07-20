4ea746367c7581911b2d6e3e43b4b826097ca3c4
erenyusufduran <eren.duran@colins.com>
2023-07-20 11:38:41 +0300

    intermediate sql select queries

--- 07-js-algorithms/10-hackerrank/SQL/01-selectQuery.sql
+++ 07-js-algorithms/10-hackerrank/SQL/01-selectQuery.sql
@@ -17,7 +17,14 @@ SELECT * FROM CITY WHERE CountryCode = 'JPN';
 SELECT NAME FROM CITY WHERE CountryCode = 'JPN';
 
 --Query a list of CITY and STATE from the STATION table.
-SELECT CITY, STATE FROM STATION;
+SELECT CITY STATE FROM STATION;
 
 --Query a list of CITY names from STATION for cities that have an even ID number. Print the results in any order, but exclude duplicates from the answer.
 SELECT DISTINCT CITY FROM STATION WHERE MOD(ID, 2) = 0 ORDER BY CITY ASC;
+
+--Find the difference between the total number of CITY entries in the table and the number of distinct CITY entries in the table.
+SELECT COUNT(CITY) - COUNT(DISTINCT CITY) FROM STATION;
+
+--Query the two cities in STATION with the shortest and longest CITY names, as well as their respective lengths (i.e.: number of characters in the name). If there is more than one smallest or largest city, choose the one that comes first when ordered alphabetically.
+SELECT CITY, LENGTH(CITY) FROM STATION ORDER BY LENGTH(CITY) DESC, CITY ASC FETCH FIRST ROW ONLY;
+SELECT CITY, LENGTH(CITY) FROM STATION ORDER BY LENGTH(CITY) ASC, CITY ASC FETCH FIRST ROW ONLY;

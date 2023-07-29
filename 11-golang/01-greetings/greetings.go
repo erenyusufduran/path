1813a6f1e4f4ffde8c3d7730deab5724bec78b1d
erenyusufduran <eren.duran@colins.com>
2023-07-29 18:35:26 +0300

    working with arrays and random

--- 11-golang/01-greetings/greetings.go
+++ 11-golang/01-greetings/greetings.go
@@ -3,6 +3,7 @@ package greetings
 import (
 	"errors"
 	"fmt"
+	"math/rand"
 )
 
 // Hello returns a greeting for the names person.
@@ -10,6 +11,33 @@ func Hello(name string) (string, error) {
 	if name == "" {
 		return "", errors.New("Empty Name")
 	}
-	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	message := fmt.Sprintf(randomFormat(), name)
 	return message, nil
+}
+
+func Hellos(names []string) (map[string]string, error) {
+	messages := make(map[string]string)
+	for _, name := range names {
+		message, err := Hello(name)
+		if err != nil {
+			return nil, err
+		}
+		messages[name] = message
+	}
+	return messages, nil
+}
+
+// randomFormat returns one of a set of greeting messages.
+
+/*
+randomFormat starts with a lowercase letter, making it accessible only to code in its own package (in other words, it's not exported).
+*/
+func randomFormat() string {
+	formats := []string{
+		"Hi, %v. Welcome!",
+		"Great to see you, %v!",
+		"Hail, %v! Well met!",
+	}
+
+	return formats[rand.Intn(len(formats))]
 }

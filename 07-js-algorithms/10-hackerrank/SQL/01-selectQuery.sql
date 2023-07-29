eb59c117c5c97c3b9f4aacf7e7649e2b7b2dd38b
erenyusufduran <eren.duran@colins.com>
2023-07-29 22:45:32 +0300

    select queries

--- 07-js-algorithms/10-hackerrank/SQL/01-selectQuery.sql
+++ 07-js-algorithms/10-hackerrank/SQL/01-selectQuery.sql
@@ -28,3 +28,7 @@ SELECT COUNT(CITY) - COUNT(DISTINCT CITY) FROM STATION;
 --Query the two cities in STATION with the shortest and longest CITY names, as well as their respective lengths (i.e.: number of characters in the name). If there is more than one smallest or largest city, choose the one that comes first when ordered alphabetically.
 SELECT CITY, LENGTH(CITY) FROM STATION ORDER BY LENGTH(CITY) DESC, CITY ASC FETCH FIRST ROW ONLY;
 SELECT CITY, LENGTH(CITY) FROM STATION ORDER BY LENGTH(CITY) ASC, CITY ASC FETCH FIRST ROW ONLY;
+
+--Query the list of CITY names starting with vowels (i.e., a, e, i, o, or u) from STATION. Your result cannot contain duplicates.
+SELECT CITY FROM STATION WHERE CITY LIKE '[a,e,i,o,u]%';
+SELECT DISTINCT(CITY) FROM STATION WHERE CITY LIKE '%[a,e,i,o,u]';

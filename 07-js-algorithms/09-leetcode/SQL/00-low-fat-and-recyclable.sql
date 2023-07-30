6cdc163adadc0ee16808faa2580df76dd4374f5c
erenyusufduran <eren.duran@colins.com>
2023-07-30 17:49:38 +0300

    select leetcode

---
+++ 07-js-algorithms/09-leetcode/SQL/00-low-fat-and-recyclable.sql
@@ -0,0 +1,9 @@
+-- In SQL, product_id is the primary key for this table.
+-- low_fats is an ENUM of type ('Y', 'N') where 'Y' means this product is low fat and 'N' means it is not.
+-- recyclable is an ENUM of types ('Y', 'N') where 'Y' means this product is recyclable and 'N' means it is not.
+
+-- Find the ids of products that are both low fat and recyclable.
+
+SELECT product_id
+FROM Products
+WHERE low_fats='Y' AND recyclable='Y'

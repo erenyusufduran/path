fd513ddf64efd755aa3fb0fb7084f1e810853ca8
erenyusufduran <eren.duran@colins.com>
2023-09-13 00:16:58 +0300

    displaying progress

--- 12-react/06-react-quiz/src/components/NextButton.js
+++ 12-react/06-react-quiz/src/components/NextButton.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const NextButton = ({ dispatch, answer }) => {
-  if (!answer) return null;
+  if (answer === null) return null;
   return (
     <button className="btn btn-ui" onClick={() => dispatch({ type: 'nextQuestion' })}>
       Next
--- 12-react/06-react-quiz/src/components/Options.js
+++ 12-react/06-react-quiz/src/components/Options.js
@@ -1,14 +1,17 @@
 import React from 'react';
 
 const Options = ({ question, dispatch, answer }) => {
+  const hasAnswered = answer !== null;
+
   return (
     <div className="options">
       {question.options.map((option, index) => (
         <button
-          className={`btn btn-option ${index === answer ? 'answer' : ''} 
-            ${answer ? (index === question.correctOption ? 'correct' : 'wrong') : ''}`}
+          className={`btn btn-option ${index === answer ? 'answer' : ''} ${
+            hasAnswered ? (index === question.correctOption ? 'correct' : 'wrong') : ''
+          }`}
           key={option}
-          disabled={answer}
+          disabled={hasAnswered}
           onClick={() => dispatch({ type: 'newAnswer', payload: index })}
         >
           {option}

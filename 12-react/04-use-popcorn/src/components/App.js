73251914b34e77702317c3a4ccc309c0a7e96b4c
erenyusufduran <eren.duran@colins.com>
2023-09-12 14:05:56 +0300

    synchronizing queries

--- 12-react/04-use-popcorn/src/components/App.js
+++ 12-react/04-use-popcorn/src/components/App.js
@@ -50,21 +50,20 @@ const average = (arr) => arr.reduce((acc, cur, i, arr) => acc + cur / arr.length
 const KEY = 'c7a85c15';
 
 export default function App() {
+  const [query, setQuery] = useState('');
   const [movies, setMovies] = useState([]);
   const [watched, setWatched] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState('');
-  const query = 'interstellar';
 
   useEffect(() => {
     const fetchMovies = async () => {
       try {
         setIsLoading(true);
+        setError('');
         const res = await fetch(`https://www.omdbapi.com/?apikey=${KEY}&s=${query}`);
-
         if (!res.ok) throw new Error('Something went wrong with fetching movies.');
         const data = await res.json();
-        
         if (data.Response === 'False') throw new Error('Movie not found');
         const filteredData = await data.Search.filter((film) => film.Poster !== 'N/A');
         setMovies(filteredData);
@@ -74,13 +73,18 @@ export default function App() {
         setIsLoading(false);
       }
     };
+    if (query.length < 3) {
+      setMovies([]);
+      setError('');
+      return;
+    }
     fetchMovies();
-  }, []);
+  }, [query]);
 
   return (
     <>
       <NavBar>
-        <Search />
+        <Search query={query} setQuery={setQuery} />
         <NumResults movies={movies} />
       </NavBar>
 
@@ -131,9 +135,7 @@ function Logo() {
   );
 }
 
-function Search() {
-  const [query, setQuery] = useState('');
-
+function Search({ query, setQuery }) {
   return (
     <input
       className="search"

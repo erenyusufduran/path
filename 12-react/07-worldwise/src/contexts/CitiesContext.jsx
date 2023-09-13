173c018ae35e19d166acbc121ab82f2e339faee2
erenyusufduran <eren.duran@colins.com>
2023-09-13 17:24:18 +0300

    finishing city view

--- 12-react/07-worldwise/src/contexts/CitiesContext.jsx
+++ 12-react/07-worldwise/src/contexts/CitiesContext.jsx
@@ -8,6 +8,7 @@ const CitiesContext = createContext();
 export const CitiesProvider = ({ children }) => {
   const [cities, setCities] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [currentCity, setCurrentCity] = useState({});
 
   useEffect(() => {
     const fetchCities = async () => {
@@ -25,7 +26,22 @@ export const CitiesProvider = ({ children }) => {
     fetchCities();
   }, []);
 
-  return <CitiesContext.Provider value={{ cities, isLoading }}>{children}</CitiesContext.Provider>;
+  const getCity = async (id) => {
+    try {
+      setIsLoading(true);
+      const response = await fetch(`${BASE_URL}/cities/${id}`);
+      const data = await response.json();
+      setCurrentCity(data);
+    } catch {
+      alert('There was an error when loading data.');
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <CitiesContext.Provider value={{ cities, isLoading, currentCity, getCity }}>{children}</CitiesContext.Provider>
+  );
 };
 
 // eslint-disable-next-line react-refresh/only-export-components

c69c0f32f231d6da695907964aeaa39fff30e040
erenyusufduran <eren.duran@colins.com>
2023-09-13 11:13:40 +0300

    BankAccount challenge

--- 12-react/06-react-quiz/src/index.js
+++ 12-react/06-react-quiz/src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App';
+// import BankAccount from './bank-challenge/BankAccount';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    {/* <BankAccount /> */}
     <App />
   </React.StrictMode>
 );

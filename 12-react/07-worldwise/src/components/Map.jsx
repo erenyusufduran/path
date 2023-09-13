28d13c350871541fa0426c7e850e01316a15fcb4
erenyusufduran <eren.duran@colins.com>
2023-09-13 17:36:47 +0300

    displaying city markers on map

--- 12-react/07-worldwise/src/components/Map.jsx
+++ 12-react/07-worldwise/src/components/Map.jsx
@@ -2,9 +2,11 @@ import { useNavigate, useSearchParams } from 'react-router-dom';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import styles from './Map.module.css';
 import { useState } from 'react';
+import { useCities } from '../contexts/CitiesContext';
 
 const Map = () => {
   const navigate = useNavigate();
+  const { cities } = useCities();
   const [mapPosition, setMapPosition] = useState([40, 0]);
 
   const [searchParams, setSearchParams] = useSearchParams();
@@ -27,17 +29,20 @@ const Map = () => {
   // );
 
   return (
-      <MapContainer className={styles.mapContainer} center={mapPosition} zoom={13} scrollWheelZoom={true}>
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
-        />
-        <Marker position={mapPosition}>
+    <MapContainer className={styles.mapContainer} center={mapPosition} zoom={13} scrollWheelZoom={true}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+      />
+      {cities.map((city) => (
+        <Marker key={city.id} position={[city.position.lat, city.position.lng]}>
           <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
+            <span>{city.emoji}</span>
+            <span>{city.cityName}</span>
           </Popup>
         </Marker>
-      </MapContainer>
+      ))}
+    </MapContainer>
   );
 };
 

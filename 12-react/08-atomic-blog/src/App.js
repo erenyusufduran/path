67a0fec76756886e3b193e631c342b22b4080cc7
erenyusufduran <eren.duran@colins.com>
2023-09-14 15:19:17 +0300

    return to the context app file

--- 12-react/08-atomic-blog/src/App.js
+++ 12-react/08-atomic-blog/src/App.js
@@ -1,4 +1,4 @@
-import { memo, useEffect, useMemo, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { PostProvider, usePosts, createRandomPost } from './PostContext';
 
 function App() {
@@ -12,13 +12,6 @@ function App() {
     [isFakeDark]
   );
 
-  const archiveOptions = useMemo(() => {
-    return {
-      show: false,
-      title: `Post archive in addition to main posts`,
-    };
-  }, []);
-
   return (
     <section>
       <button onClick={() => setIsFakeDark((isFakeDark) => !isFakeDark)} className="btn-fake-dark-mode">
@@ -27,7 +20,7 @@ function App() {
       <PostProvider>
         <Header />
         <Main />
-        <Archive archiveOptions={archiveOptions} />
+        <Archive />
         <Footer />
       </PostProvider>
     </section>
@@ -107,21 +100,18 @@ function List() {
   const { posts } = usePosts();
 
   return (
-    <>
-      <ul>
-        {posts.map((post, i) => (
-          <li key={i}>
-            <h3>{post.title}</h3>
-            <p>{post.body}</p>
-          </li>
-        ))}
-      </ul>
-      {/* <Test /> */}
-    </>
+    <ul>
+      {posts.map((post, i) => (
+        <li key={i}>
+          <h3>{post.title}</h3>
+          <p>{post.body}</p>
+        </li>
+      ))}
+    </ul>
   );
 }
 
-const Archive = memo(({ archiveOptions }) => {
+function Archive() {
   const { onAddPost } = usePosts();
 
   // Here we don't need the setter function. We're only using state to store these posts because the callback function passed into useState (which generates the posts) is only called once, on the initial render. So we use this trick as an optimization technique, because if we just used a regular variable, these posts would be re-created on every render. We could also move the posts outside the components, but I wanted to show you this trick 😉
@@ -130,11 +120,11 @@ const Archive = memo(({ archiveOptions }) => {
     Array.from({ length: 10000 }, () => createRandomPost())
   );
 
-  const [showArchive, setShowArchive] = useState(archiveOptions.show);
+  const [showArchive, setShowArchive] = useState(false);
 
   return (
     <aside>
-      <h2>{archiveOptions.title}</h2>
+      <h2>Post archive</h2>
       <button onClick={() => setShowArchive((s) => !s)}>
         {showArchive ? 'Hide archive posts' : 'Show archive posts'}
       </button>
@@ -153,7 +143,7 @@ const Archive = memo(({ archiveOptions }) => {
       )}
     </aside>
   );
-});
+}
 
 function Footer() {
   return <footer>&copy; by The Atomic Blog ✌️</footer>;

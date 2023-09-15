bbc701e6f439ef7303bde7844808b7bcd4650557
erenyusufduran <eren.duran@colins.com>
2023-09-15 12:28:17 +0300

    not rtk type thunk, but thunk applied

--- 12-react/10-redux-intro/src/features/accounts/accountSlice.js
+++ 12-react/10-redux-intro/src/features/accounts/accountSlice.js
@@ -14,6 +14,7 @@ const accountSlice = createSlice({
     deposit(state, action) {
       // We can mutate it
       state.balance += action.payload;
+      state.isLoading = false;
     },
     withdraw(state, action) {
       state.balance -= action.payload;
@@ -30,14 +31,29 @@ const accountSlice = createSlice({
         state.balance += action.payload.amount;
       },
     },
-    payLoan(state, action) {
+    payLoan(state) {
       state.balance -= state.loan;
       state.laon = 0;
       state.loanPurpose = '';
     },
+    convertingCurrency(state) {
+      state.isLoading = true;
+    },
   },
 });
 
-export const { deposit, withdraw, requestLoan, payLoan } = accountSlice.actions;
+export function deposit(amount, currency) {
+  if (currency === 'USD') return { type: 'account/deposit', payload: amount };
+
+  return async function (dispatch, getState) {
+    dispatch({ type: 'account/convertingCurrency' });
+    const res = await fetch(`https://api.frankfurter.app/latest?amount=${amount}&from=${currency}&to=USD`);
+    const data = await res.json();
+    const converted = data.rates.USD;
+    dispatch({ type: 'account/deposit', payload: converted });
+  };
+}
+
+export const { withdraw, requestLoan, payLoan } = accountSlice.actions;
 
 export default accountSlice.reducer;

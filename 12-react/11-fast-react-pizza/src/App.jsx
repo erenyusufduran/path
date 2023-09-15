1c2b912bf266678a9bf850bdb36a94d194d84117
erenyusufduran <eren.duran@colins.com>
2023-09-15 16:39:07 +0300

    handling errors with errorElement in react-router

--- 12-react/11-fast-react-pizza/src/App.jsx
+++ 12-react/11-fast-react-pizza/src/App.jsx
@@ -1,15 +1,18 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
 import Home from './ui/Home';
+import AppLayout from './ui/AppLayout';
+import Error from './ui/Error';
+
 import Menu, { loader as menuLoader } from './features/menu/Menu';
 import Cart from './features/cart/Cart';
 import CreateOrder from './features/order/CreateOrder';
 import Order from './features/order/Order';
-import AppLayout from './ui/AppLayout';
 
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
+    errorElement: <Error />,
     children: [
       { path: '/', element: <Home /> },
       { path: '/menu', element: <Menu />, loader: menuLoader },

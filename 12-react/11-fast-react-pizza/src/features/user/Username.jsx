d948b098f968faf558463440713c28145c21598f
erenyusufduran <eren.duran@colins.com>
2023-09-15 20:10:11 +0300

    flex

--- 12-react/11-fast-react-pizza/src/features/user/Username.jsx
+++ 12-react/11-fast-react-pizza/src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 const Username = () => {
-  return <div className="text-sm font-semibold hidden">Jonas</div>;
+  return <div className="text-sm font-semibold hidden md:block">Jonas</div>;
 };
 
 export default Username;

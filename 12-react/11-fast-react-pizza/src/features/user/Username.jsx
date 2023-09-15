c10df019adcc3612fda32984d1a9f06bafa8a2f4
erenyusufduran <eren.duran@colins.com>
2023-09-15 19:53:41 +0300

    spacing, borders

--- 12-react/11-fast-react-pizza/src/features/user/Username.jsx
+++ 12-react/11-fast-react-pizza/src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 const Username = () => {
-  return <div className="text-sm font-semibold">Jonas</div>;
+  return <div className="text-sm font-semibold hidden">Jonas</div>;
 };
 
 export default Username;
--- 12-react/11-fast-react-pizza/src/ui/Home.jsx
+++ 12-react/11-fast-react-pizza/src/ui/Home.jsx
@@ -1,11 +1,14 @@
+import CreateUser from "../features/user/CreateUser";
+
 const Homes = () => {
   return (
-    <div>
-      <h1 className="text-center text-xl font-semibold">
+    <div className="my-10 text-center">
+      <h1 className="text-center text-xl font-semibold mb-8">
         The best pizza.
         <br />
         <span className="text-yellow-500">Straight out of the oven, straight to you.</span>
       </h1>
+      <CreateUser />
     </div>
   );
 };

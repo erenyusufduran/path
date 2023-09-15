046c7be142fa95a98eae3d7c29968cf46ca65030
erenyusufduran <eren.duran@colins.com>
2023-09-15 20:29:32 +0300

    button style

--- 12-react/11-fast-react-pizza/src/features/order/CreateOrder.jsx
+++ 12-react/11-fast-react-pizza/src/features/order/CreateOrder.jsx
@@ -32,7 +32,7 @@ function CreateOrder() {
   const navigation = useNavigation();
   const isSubmitting = navigation.state === 'submitting';
   const formErrors = useActionData();
-  
+
   const cart = fakeCart;
 
   return (
@@ -74,7 +74,12 @@ function CreateOrder() {
 
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
-          <button disabled={isSubmitting}>{isSubmitting ? 'Placing order...' : 'Order now'}</button>
+          <button
+            className="tracking-wide rounded-full bg-yellow-400 uppercase font-semibold text-stone-800 py-3 px-4 inline-block hover:bg-yellow-300 transition-colors duration-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed"
+            disabled={isSubmitting}
+          >
+            {isSubmitting ? 'Placing order...' : 'Order now'}
+          </button>
         </div>
       </Form>
     </div>

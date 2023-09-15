eecd2a430c3e7fc08b91388269532afffb07e86a
erenyusufduran <eren.duran@colins.com>
2023-09-16 02:37:38 +0300

    styling the cart

--- 12-react/11-fast-react-pizza/src/features/cart/Cart.jsx
+++ 12-react/11-fast-react-pizza/src/features/cart/Cart.jsx
@@ -1,42 +1,50 @@
 import LinkButton from '../../ui/LinkButton';
 import Button from '../../ui/Button';
+import CartItem from './CartItem';
 
-// const fakeCart = [
-//   {
-//     pizzaId: 12,
-//     name: 'Mediterranean',
-//     quantity: 2,
-//     unitPrice: 16,
-//     totalPrice: 32,
-//   },
-//   {
-//     pizzaId: 6,
-//     name: 'Vegetale',
-//     quantity: 1,
-//     unitPrice: 13,
-//     totalPrice: 13,
-//   },
-//   {
-//     pizzaId: 11,
-//     name: 'Spinach and Mushroom',
-//     quantity: 1,
-//     unitPrice: 15,
-//     totalPrice: 15,
-//   },
-// ];
+const fakeCart = [
+  {
+    pizzaId: 12,
+    name: 'Mediterranean',
+    quantity: 2,
+    unitPrice: 16,
+    totalPrice: 32,
+  },
+  {
+    pizzaId: 6,
+    name: 'Vegetale',
+    quantity: 1,
+    unitPrice: 13,
+    totalPrice: 13,
+  },
+  {
+    pizzaId: 11,
+    name: 'Spinach and Mushroom',
+    quantity: 1,
+    unitPrice: 15,
+    totalPrice: 15,
+  },
+];
 
 function Cart() {
-  // const cart = fakeCart;
+  const cart = fakeCart;
 
   return (
-    <div>
+    <div className="px-4 py-3">
       <LinkButton to="/menu" className="text-sm text-blue-500 hover:text-blue-700 hover:underline">
         &larr; Back to menu
       </LinkButton>
-      <h2>Your cart, %NAME%</h2>
-      <div>
-        <Button type="primary" to="order/new">Order pizzas</Button>
-        <button>Clear cart</button>
+      <h2 className="mt-7 text-lg font-semibold">Your cart, %NAME%</h2>
+      <ul className="divide-y divide-stone-200 border-b mt-3">
+        {cart.map((item) => (
+          <CartItem item={item} key={item.key} />
+        ))}
+      </ul>
+      <div className="mt-6 space-x-2">
+        <Button type="primary" to="order/new">
+          Order pizzas
+        </Button>
+        <Button type="secondary">Clear cart</Button>
       </div>
     </div>
   );
--- 12-react/11-fast-react-pizza/src/ui/Button.jsx
+++ 12-react/11-fast-react-pizza/src/ui/Button.jsx
@@ -2,11 +2,12 @@ import { Link } from 'react-router-dom';
 
 const Button = ({ children, disabled, to, type }) => {
   const base =
-    'tracking-wide rounded-full bg-yellow-400 uppercase font-semibold text-stone-800 inline-block hover:bg-yellow-300 transition-colors duration-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed';
+    'tracking-wide rounded-full uppercase font-semibold inline-block transition-colors duration-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed';
 
   const styles = {
-    primary: base + ' px-4 py3 md:px-6 md:py-4',
-    small: base + ' py-2 px-4 md:px-5 md:py-2.5 text-xs',
+    primary: base + ' focus:ring-yellow-300 text-stone-800 bg-yellow-400 hover:bg-yellow-300 px-4 py-3 md:px-6 md:py-4',
+    small: base + ' focus:ring-yellow-300 text-stone-800 bg-yellow-400 hover:bg-yellow-300 py-2 px-4 md:px-5 md:py-2.5 text-xs',
+    secondary: base + ' hover:bg-stone-300 focus:bg-stone-300 hover:text-stone-800 focus:ring-stone-200 border-2 border-stone-300 px-4 py-2.5 md:px-6 md:py-3.5',
   };
 
   if (to) {

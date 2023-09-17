30113f220a1279aeff2d7ce198488d0e635a9265
erenyusufduran <eren.duran@colins.com>
2023-09-17 17:07:35 +0300

    fix link issue

--- 12-react/11-fast-react-pizza/src/features/cart/Cart.jsx
+++ 12-react/11-fast-react-pizza/src/features/cart/Cart.jsx
@@ -41,7 +41,7 @@ function Cart() {
         ))}
       </ul>
       <div className="mt-6 space-x-2">
-        <Button type="primary" to="order/new">
+        <Button type="primary" to="/order/new">
           Order pizzas
         </Button>
         <Button type="secondary">Clear cart</Button>

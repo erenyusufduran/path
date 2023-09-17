6952de2660329cc8130e24690302412df0c577fc
erenyusufduran <eren.duran@colins.com>
2023-09-17 17:17:03 +0300

    modeling the user state with redux toolkit

--- 12-react/11-fast-react-pizza/src/features/user/Username.jsx
+++ 12-react/11-fast-react-pizza/src/features/user/Username.jsx
@@ -1,5 +1,10 @@
+import { useSelector } from 'react-redux';
+
 const Username = () => {
-  return <div className="text-sm font-semibold hidden md:block">Jonas</div>;
+  const username = useSelector((state) => state.user.username);
+  if (!username) return null;
+
+  return <div className="text-sm font-semibold hidden md:block">{username}</div>;
 };
 
 export default Username;

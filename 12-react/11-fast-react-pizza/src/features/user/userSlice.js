fae1ce8225447de82247c4fa3ccb9ea20f9611a7
erenyusufduran <eren.duran@colins.com>
2023-09-17 17:23:53 +0300

    reading and updating the user state

--- 12-react/11-fast-react-pizza/src/features/user/userSlice.js
+++ 12-react/11-fast-react-pizza/src/features/user/userSlice.js
@@ -1,7 +1,24 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+// const getPosition = () => {
+//   return new Promise((resolve, reject) => {
+//     navigator.geolocation.getCurrentPosition(resolve, reject);
+//   });
+// };
+
+// const fetchAddress = async () => {
+//   const positionObj = await getPosition();
+//   const position = {
+//     latitude: positionObj.coords.latitude,
+//     longitude: positionObj.coords.longitude,
+//   };
+//   const addressObj = await getAddress(position);
+//   const address = `${addressObj?.locality} ${addressObj?.city} ${addressObj?.postcode}, ${addressObj?.countryName}`;
+//   return { position, address };
+// };
+
 const initialState = {
-  username: 'Gökşen',
+  username: '',
 };
 
 const userSlice = createSlice({
--- 12-react/11-fast-react-pizza/src/ui/Home.jsx
+++ 12-react/11-fast-react-pizza/src/ui/Home.jsx
@@ -1,6 +1,10 @@
-import CreateUser from "../features/user/CreateUser";
+import { useSelector } from 'react-redux';
+import CreateUser from '../features/user/CreateUser';
+import Button from './Button';
 
 const Homes = () => {
+  const username = useSelector((state) => state.user.username);
+
   return (
     <div className="my-10 sm:my-16 text-center px-4">
       <h1 className="text-center text-xl font-semibold mb-8 md:text-3xl">
@@ -8,7 +12,13 @@ const Homes = () => {
         <br />
         <span className="text-yellow-500">Straight out of the oven, straight to you.</span>
       </h1>
-      <CreateUser />
+      {username === '' ? (
+        <CreateUser />
+      ) : (
+        <Button to="/menu" type="primary">
+          Continue ordering, {username}
+        </Button>
+      )}
     </div>
   );
 };

5aecda35220b225a01447b6cc0393282479e26a2
erenyusufduran <eren.duran@colins.com>
2023-09-18 20:08:41 +0300

    building more reusable components

--- 12-react/the-wild-oasis/src/App.jsx
+++ 12-react/the-wild-oasis/src/App.jsx
@@ -3,9 +3,9 @@ import GlobalStyles from './styles/GlobalStyles';
 import Button from './ui/Button';
 import Input from './ui/Input';
 import Heading from './ui/Heading';
+import Row from './ui/Row';
 
 const StyledApp = styled.div`
-  background-color: orangered;
   padding: 20px;
 `;
 
@@ -14,18 +14,27 @@ function App() {
     <>
       <GlobalStyles />
       <StyledApp>
-        <Heading as="h1">The Wild Oasis</Heading>
-        <Heading as="h2">Check in and out</Heading>
+        <Row>
+          <Row type="horizontal">
+            <Heading as="h1">The Wild Oasis</Heading>
+            <div>
+              <Heading as="h2">Check in and out</Heading>
 
-        <Button onClick={() => alert('Check in')}>Check in</Button>
-        <Button onClick={() => alert('Check in')}>Check in</Button>
-        <Input type="number" placeholder="Number of guests" />
+              <Button onClick={() => alert('Check in')}>Check in</Button>
+              <Button variation="danger" size="small" onClick={() => alert('Check in')}>
+                Check in
+              </Button>
+            </div>
+          </Row>
+          <Row>
+            <Heading as="h3">Form</Heading>
+            <form>
+              <Input type="number" placeholder="Number of guests" />
+              <Input type="number" placeholder="Number of guests" />
+            </form>
+          </Row>
+        </Row>
       </StyledApp>
-
-      <Heading as="h3">Form</Heading>
-      <Button onClick={() => alert('Check in')}>Check in</Button>
-      <Button onClick={() => alert('Check in')}>Check in</Button>
-      <Input type="number" placeholder="Number of guests" />
     </>
   );
 }

5f96f5b7a53fd3fb54c9fa6ec9891d9186b5eed5
erenyusufduran <eren.duran@colins.com>
2023-09-19 00:52:18 +0300

    supabase connection

---
+++ 12-react/the-wild-oasis/src/services/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://lfazbrqzuauxduyiypir.supabase.co';
+const supabaseKey =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxmYXpicnF6dWF1eGR1eWl5cGlyIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTUwNzIyMDcsImV4cCI6MjAxMDY0ODIwN30.1eGWnAPyjKpxTpG6U-yg77o78Fe2E8mj1rZs6YmBxTU';
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;

142f069e201714a0f07aa6aef8bc849e871348a3
erenyusufduran <eren.duran@colins.com>
2023-09-19 17:34:42 +0300

    setting up react query

--- 12-react/the-wild-oasis/src/App.jsx
+++ 12-react/the-wild-oasis/src/App.jsx
@@ -1,20 +1,28 @@
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import GlobalStyles from './styles/GlobalStyles';
-
 import Dashboard from './pages/Dashboard';
-import Account from './pages/Account';
 import Bookings from './pages/Bookings';
 import Cabins from './pages/Cabins';
+import Users from './pages/Users';
+import Settings from './pages/Settings';
+import Account from './pages/Account';
 import Login from './pages/Login';
 import PageNotFound from './pages/PageNotFound';
-import Settings from './pages/Settings';
-import Users from './pages/Users';
 import AppLayout from './ui/AppLayout';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: { staleTime: 60 * 1000 },
+  },
+});
 
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
@@ -23,15 +31,16 @@ function App() {
             <Route path="dashboard" element={<Dashboard />} />
             <Route path="bookings" element={<Bookings />} />
             <Route path="cabins" element={<Cabins />} />
-            <Route path="users" element={<Account />} />
+            <Route path="users" element={<Users />} />
             <Route path="settings" element={<Settings />} />
-            <Route path="account" element={<Users />} />
+            <Route path="account" element={<Account />} />
           </Route>
+
           <Route path="login" element={<Login />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
-    </>
+    </QueryClientProvider>
   );
 }
 

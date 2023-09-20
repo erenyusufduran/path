cfe1348e7c0c20d91f2f9e6a66b4807ba9907b15
erenyusufduran <eren.duran@colins.com>
2023-09-20 13:57:18 +0300

    toast notifications

--- 12-react/the-wild-oasis/src/features/cabins/CabinRow.jsx
+++ 12-react/the-wild-oasis/src/features/cabins/CabinRow.jsx
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import { formatCurrency } from '../../utils/helpers';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { deleteCabin } from '../../services/apiCabins';
+import toast from 'react-hot-toast';
 
 const TableRow = styled.div`
   display: grid;
@@ -50,17 +51,17 @@ const CabinRow = ({ cabin }) => {
   const { isLoading: isDeleting, mutate } = useMutation({
     mutationFn: deleteCabin,
     onSuccess: () => {
-      alert("Cabin succesfully deleted");
+      toast.success("Cabin succesfully deleted");
       queryClient.invalidateQueries({ queryKey: ['cabins'] });
     },
-    onError: (err) => alert(err.message),
+    onError: (err) => toast.error(err.message),
   });
 
   return (
     <TableRow role="row">
       <Img src={image} />
       <Cabin>{name}</Cabin>
-      <div>Fits up tp {maxCapacity} guests</div>
+      <div>Fits up to {maxCapacity} guests</div>
       <Price>{formatCurrency(regularPrice)}</Price>
       <Discount>{formatCurrency(discount)}</Discount>
       <button disabled={isDeleting} onClick={() => mutate(cabinId)}>

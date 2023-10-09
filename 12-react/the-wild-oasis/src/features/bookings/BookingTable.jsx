a4191c35bdde863a161736e980d72ba56b0173dc
erenyusufduran <eren.duran@colins.com>
2023-10-09 16:12:53 +0300

    reusable pagination component

--- 12-react/the-wild-oasis/src/features/bookings/BookingTable.jsx
+++ 12-react/the-wild-oasis/src/features/bookings/BookingTable.jsx
@@ -4,6 +4,7 @@ import Menus from '../../ui/Menus';
 import Empty from '../../ui/Empty';
 import { useBookings } from './useBookings';
 import Spinner from '../../ui/Spinner';
+import Pagination from '../../ui/Pagination';
 
 function BookingTable() {
   const { bookings, isLoading } = useBookings();
@@ -24,6 +25,10 @@ function BookingTable() {
         </Table.Header>
 
         <Table.Body data={bookings} render={(booking) => <BookingRow key={booking.id} booking={booking} />} />
+      
+        <Table.Footer>
+          <Pagination count={45} />
+        </Table.Footer>
       </Table>
     </Menus>
   );

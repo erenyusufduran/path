e8199d2c1db2ba81434e01c857b91415261c5706
erenyusufduran <eren.duran@colins.com>
2023-10-09 15:34:53 +0300

    api side filtering

--- 12-react/the-wild-oasis/src/features/bookings/useBookings.js
+++ 12-react/the-wild-oasis/src/features/bookings/useBookings.js
@@ -1,7 +1,18 @@
 import { useQuery } from '@tanstack/react-query';
+import { useSearchParams } from 'react-router-dom';
 import { getBookings } from '../../services/apiBookings';
 
 export const useBookings = () => {
-  const { isLoading, data: bookings, error } = useQuery({ queryKey: ['bookings'], queryFn: getBookings });
+  const [searchParams] = useSearchParams();
+
+  // FILTER
+  const filterValue = searchParams.get('status');
+  const filter = !filterValue || filterValue === 'all' ? null : { field: 'status', value: filterValue, method: "eq" };
+
+  const {
+    isLoading,
+    data: bookings,
+    error,
+  } = useQuery({ queryKey: ['bookings', filter], queryFn: () => getBookings({ filter }) });
   return { isLoading, bookings, error };
 };

8df3822a5d70eb0e21794f8fa513f70da4ccecbd
erenyusufduran <eren.duran@colins.com>
2023-10-09 09:49:30 +0300

    client side filtering

--- 12-react/the-wild-oasis/src/features/cabins/CabinTable.jsx
+++ 12-react/the-wild-oasis/src/features/cabins/CabinTable.jsx
@@ -1,3 +1,5 @@
+import { useSearchParams } from 'react-router-dom';
+
 import Spinner from '../../ui/Spinner';
 import CabinRow from './CabinRow';
 
@@ -7,8 +9,16 @@ import Menus from '../../ui/Menus';
 
 const CabinTable = () => {
   const { isLoading, cabins } = useCabins();
+  const [searchParams] = useSearchParams();
 
   if (isLoading) return <Spinner />;
+
+  const filterValue = searchParams.get('discount') || 'all';
+  let filteredCabins;
+  if (filterValue === 'all') filteredCabins = cabins;
+  if (filterValue === 'no-discount') filteredCabins = cabins.filter((cabin) => cabin.discount === 0);
+  if (filterValue === 'with-discount') filteredCabins = cabins.filter((cabin) => cabin.discount > 0);
+
   return (
     <Menus>
       <Table columns="0.6fr 1.8fr 2.2fr 1fr 1fr 1fr">
@@ -19,7 +29,7 @@ const CabinTable = () => {
           <div>Price</div>
           <div>Discount</div>
         </Table.Header>
-        <Table.Body data={cabins} render={(cabin) => <CabinRow cabin={cabin} key={cabin.id} />} />
+        <Table.Body data={filteredCabins} render={(cabin) => <CabinRow cabin={cabin} key={cabin.id} />} />
       </Table>
     </Menus>
   );

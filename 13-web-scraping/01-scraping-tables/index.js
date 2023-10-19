c7ed87d8ae7ed8d5438aaa33bd6057199def6efd
erenyusufduran <eren.duran@colins.com>
2023-10-19 15:19:16 +0300

    webscraping from another site

--- 13-web-scraping/01-scraping-tables/index.js
+++ 13-web-scraping/01-scraping-tables/index.js
@@ -10,7 +10,7 @@ async function oldMain() {
   });
 }
 
-async function main() {
+async function tableMain() {
   const result = await request.get('https://codingwithstefan.com/table-example');
   const $ = cheerio.load(result);
 
@@ -24,6 +24,28 @@ async function main() {
 
     const scrapedRow = { company, contact, country };
     scrapedRows.push(scrapedRow);
+  });
+
+  console.log(scrapedRows);
+}
+
+async function main() {
+  const result = await request.get('https://www.hunkarturizm.com/turbulteni');
+  const $ = cheerio.load(result);
+
+  const scrapedRows = [];
+  $('body > div.turtakvim > div > div > table > tbody > tr').each((index, element) => {
+    const tds = $(element).find('td');
+    const tourName = $(tds[0]).text();
+    const goDate = $(tds[1]).text();
+    const returnDate = $(tds[2]).text();
+    const price = $(tds[3]).text();
+    const quota = $(tds[4]).text();
+
+    if (tourName !== 'Bu ay için turumuz bulunmamaktadır.') {
+      const scrapedRow = { tourName, goDate, returnDate, price, quota };
+      scrapedRows.push(scrapedRow);
+    }
   });
 
   console.log(scrapedRows);

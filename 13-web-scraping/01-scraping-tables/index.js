ea678b008123421da35ace169e5ed6173ef17714
erenyusufduran <eren.duran@colins.com>
2023-10-19 14:30:08 +0300

    scraping company names

--- 13-web-scraping/01-scraping-tables/index.js
+++ 13-web-scraping/01-scraping-tables/index.js
@@ -1,12 +1,25 @@
 const request = require('request-promise');
 const cheerio = require('cheerio');
 
-async function main() {
+async function oldMain() {
   const result = await request.get('https://codingwithstefan.com/table-example');
   const $ = cheerio.load(result);
 
   $('body > table > tbody > tr > td').each((index, element) => {
     console.log($(element).text());
+  });
+}
+
+async function main() {
+  const result = await request.get('https://codingwithstefan.com/table-example');
+  const $ = cheerio.load(result);
+
+  $('body > table > tbody > tr').each((index, element) => {
+    const tds = $(element).find('td');
+    const company = $(tds[0]).text();
+    console.log(company);
+
+    // console.log($($(element).find('td')[0]).text());
   });
 }
 

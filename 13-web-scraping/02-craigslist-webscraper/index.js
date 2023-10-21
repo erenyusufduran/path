d4f5473db76d1c1d29c1bd47cd22f2a840c605fe
erenyusufduran <eren.duran@colins.com>
2023-10-21 18:36:41 +0300

    scraping description urls

--- 13-web-scraping/02-craigslist-webscraper/index.js
+++ 13-web-scraping/02-craigslist-webscraper/index.js
@@ -8,7 +8,9 @@ async function main() {
   await page.goto('https://sfbay.craigslist.org/search/sof', { waitUntil: 'networkidle0' });
   const html = await page.content();
   const $ = cheerio.load(html);
-  $('.title-blob').each((_, element) => console.log($(element).text()));
+  $('.posting-title').each((_, element) => console.log($(element).text()));
+  $('.posting-title').each((_, element) => console.log($(element).attr("href")));
+
 }
 
 main();

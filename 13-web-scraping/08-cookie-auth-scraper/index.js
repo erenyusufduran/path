c5e5f8a9e862e2bea7106db62c09a2c0fd0c4941
erenyusufduran <eren.duran@colins.com>
2023-11-22 19:32:27 +0300

    cookie/session authentication

---
+++ 13-web-scraping/08-cookie-auth-scraper/index.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+let request = require('request-promise');
+const cookieJar = request.jar();
+request = request.defaults({ jar: cookieJar });
+
+async function main() {
+  const result = await request.get('https://internshala.com/');
+  const cookieString = cookieJar.getCookieString('https://internshala.com/');
+  const splittedByCsrfCookieName = cookieString.split("csrf_cookie_name=");
+  const csrf_test_name = splittedByCsrfCookieName[1].split(";")[0];
+
+  const loginResult = await request.post('https://internshala.com/login/verify_ajax/user', {
+    form: {
+      csrf_test_name,
+      email: 'erenyusufduran1905@gmail.com',
+      password: 'EREN123',
+    },
+  });
+  const matches = await request.get("https://internshala.com/internships/matching-preferences");
+  fs.writeFileSync("./matches.html", matches);
+  
+  console.log(loginResult)
+}
+
+main();

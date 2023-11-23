3cf9ec6a4cc36faa12b849ef318e3b2318edb7dc
erenyusufduran <eren.duran@colins.com>
2023-11-23 15:59:36 +0300

    scrape poster url

--- 13-web-scraping/09-nightmare-scraper/index.js
+++ 13-web-scraping/09-nightmare-scraper/index.js
@@ -1,27 +1,44 @@
 const request = require('request-promise');
 const cheerio = require('cheerio');
 
-const sampleResult = {
-  title: 'Bohemian Rhapsody',
-  rank: 1,
-  imdbRating: 8.4,
-  descriptionUrl: '',
-  posterUrl: '',
-};
+const baseUrl = "'https://www.imdb.com";
 
 async function scrapeTitlesRanksAndRatings() {
-  const result = await request.get('https://www.imdb.com/chart/moviemeter?ref_=nc_mc_mpm');
-  const $ = await cheerio.load(result);
+  const result = await request.get(`${baseUrl}/chart/moviemeter?ref_=nc_mc_mpm`);
+  const $ = cheerio.load(result);
 
   const movies = $('li.ipc-metadata-list-summary-item')
     .map((i, element) => {
       const title = $(element).find('.ipc-title__text').text();
-      const descriptionUrl = "https://www.imdb.com" + $(element).find('a').attr('href');
+      const descriptionUrl = baseUrl + $(element).find('a').attr('href');
       const imdbRating = $(element).find('.ipc-rating-star').text().split('(')[0].trim();
       return { title, descriptionUrl, imdbRating, rank: i };
     })
     .get();
-  console.log(movies);
+  return movies;
 }
 
-scrapeTitlesRanksAndRatings();
+async function scrapePosterUrl(movies) {
+  try {
+    const moviesWithPosterUrls = await Promise.all(
+      movies.map(async (movie) => {
+        const html = await request.get(movie.descriptionUrl);
+        const $ = cheerio.load(html);
+        movie.posterUrl = baseUrl + $('.ipc-lockup-overlay').attr('href');
+        return movie;
+      })
+    );
+    return moviesWithPosterUrls;
+  } catch (error) {
+    console.error(error);
+    return movies;
+  }
+}
+
+async function main() {
+  const movies = await scrapeTitlesRanksAndRatings();
+  const moviesWithPosters = await scrapePosterUrl(movies);
+  console.log(moviesWithPosters);
+}
+
+main();

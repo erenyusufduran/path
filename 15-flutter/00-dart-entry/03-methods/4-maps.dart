f2e091596a1ff137287edde4021bea40fc0246d6
erenyusufduran <eren.duran@colins.com>
2023-11-26 19:47:41 +0300

    maps

---
+++ 15-flutter/00-dart-entry/03-methods/4-maps.dart
@@ -0,0 +1,25 @@
+void main(List<String> args) {
+  Map<String, dynamic> map = Map();
+  var map2 = <String, dynamic>{};
+
+  map["id"] = 5;
+  map["name"] = "eren";
+  map["color"] = "blue";
+
+  print(map);
+
+  var newMap = Map.from({"value": "new"});
+  var mapFromEntries = Map.fromEntries(map.entries);
+  print(mapFromEntries);
+
+  var list = [1, 2, 3, 4];
+  var mapFromIterable = Map<String, String>.fromIterable(list,
+      key: (item) => "$item", value: (item) => "${item * 2}");
+  print(mapFromIterable);
+
+  map.update("newId", (value) => value * 3, ifAbsent: () => 100);
+
+  map.putIfAbsent("surname", () => "Duran");
+  print(map);
+
+}

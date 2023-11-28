496b352c9a0cdc093726c6b7d338a44ab261e772
erenyusufduran <eren.duran@colins.com>
2023-11-28 12:57:57 +0300

    wait until data has came

--- 15-flutter/00-dart-entry/05-async/async_programming.dart
+++ 15-flutter/00-dart-entry/05-async/async_programming.dart
@@ -1,13 +1,18 @@
 import 'dart:io';
 
-void main(List<String> args) {
-  print("Hi there");
-  Future<String> result = longTransaction();
-  result
-      .then((String value) => print(value))
-      .catchError((error) => print(error))
-      .whenComplete(() => print("Bread operations is completed!"));
-  print("Is children come home?");
+void main(List<String> args) async {
+  // print("Hi there");
+  // Future<String> result = longTransaction();
+  // result
+  //     .then((String value) => print(value))
+  //     .catchError((error) => print(error))
+  //     .whenComplete(() => print("Bread operations is completed!"));
+  // print("Is children come home?");
+
+  print("Getting personal info");
+  String person = await getPersonalInfo();
+  print(person);
+  print("Transaction is over");
 }
 
 Future<String> longTransaction() {
@@ -15,4 +20,8 @@ Future<String> longTransaction() {
     // throw Exception("There is no bread");
     return "Kid will come home with bread";
   });
+}
+
+Future<String> getPersonalInfo() {
+  return Future.delayed(Duration(seconds: 3), () => "Eren");
 }

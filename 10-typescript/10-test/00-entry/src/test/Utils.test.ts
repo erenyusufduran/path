61323abb43226dea97b559d68fbdc390cbc84cee
erenyusufduran <eren.duran@colins.com>
2023-12-02 04:57:58 +0300

    multiple test structure

--- 10-typescript/10-test/00-entry/src/test/Utils.test.ts
+++ 10-typescript/10-test/00-entry/src/test/Utils.test.ts
@@ -12,22 +12,31 @@ describe('Utils test suite', () => {
     expect(actual).toBe(expected);
   });
 
-  it('Should return info for valid string', () => {
-    const actual = getStringInfo('My-String');
-
-    expect(actual.lowerCase).toBe('my-string');
-    expect(actual.extraInfo).toEqual({}); // when we compare objects, use toEqual
-
-    expect(actual.characters.length).toBe(9);
-    expect(actual.characters).toHaveLength(9);
-
-    expect(actual.characters).toEqual(['M', 'y', '-', 'S', 't', 'r', 'i', 'n', 'g']);
-    expect(actual.characters).toContain<string>('M');
-    expect(actual.characters).toEqual(expect.arrayContaining(['S', 't', 'r', 'i', 'n', 'g', 'M', 'y', '-']));
-
-    expect(actual.extraInfo).not.toBe(undefined);
-    expect(actual.extraInfo).not.toBeUndefined();
-    expect(actual.extraInfo).toBeDefined();
-    expect(actual.extraInfo).toBeTruthy();
+  describe('getStringInfo for arg My-String should', () => {
+    test('Return right length', () => {
+      const actual = getStringInfo('My-String');
+      expect(actual.characters).toHaveLength(9);
+    });
+    test('Return right lowercase', () => {
+      const actual = getStringInfo('My-String');
+      expect(actual.lowerCase).toBe('my-string');
+    });
+    test('Return right uppercase', () => {
+      const actual = getStringInfo('My-String');
+      expect(actual.upperCase).toBe('MY-STRING');
+    });
+    test('Return right characters', () => {
+      const actual = getStringInfo('My-String');
+      expect(actual.characters).toEqual(['M', 'y', '-', 'S', 't', 'r', 'i', 'n', 'g']);
+      expect(actual.characters).toContain<string>('M');
+      expect(actual.characters).toEqual(expect.arrayContaining(['S', 't', 'r', 'i', 'n', 'g', 'M', 'y', '-']));
+    });
+    test('Return right extra info', () => {
+      const actual = getStringInfo('My-String');
+      expect(actual.extraInfo).not.toBe(undefined);
+      expect(actual.extraInfo).not.toBeUndefined();
+      expect(actual.extraInfo).toBeDefined();
+      expect(actual.extraInfo).toBeTruthy();
+    });
   });
 });

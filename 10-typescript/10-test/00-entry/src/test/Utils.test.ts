d5862f4896dac0ff396a7cb1b1fc62b9829c42da
erenyusufduran <eren.duran@colins.com>
2023-12-02 04:52:51 +0300

    jest assertions and matchers

--- 10-typescript/10-test/00-entry/src/test/Utils.test.ts
+++ 10-typescript/10-test/00-entry/src/test/Utils.test.ts
@@ -1,4 +1,4 @@
-import { toUpperCase } from '../app/Utils';
+import { getStringInfo, toUpperCase } from '../app/Utils';
 
 describe('Utils test suite', () => {
   test('Should return uppercase of valid string', () => {
@@ -10,5 +10,24 @@ describe('Utils test suite', () => {
 
     // assert:
     expect(actual).toBe(expected);
+  });
+
+  it('Should return info for valid string', () => {
+    const actual = getStringInfo('My-String');
+
+    expect(actual.lowerCase).toBe('my-string');
+    expect(actual.extraInfo).toEqual({}); // when we compare objects, use toEqual
+
+    expect(actual.characters.length).toBe(9);
+    expect(actual.characters).toHaveLength(9);
+
+    expect(actual.characters).toEqual(['M', 'y', '-', 'S', 't', 'r', 'i', 'n', 'g']);
+    expect(actual.characters).toContain<string>('M');
+    expect(actual.characters).toEqual(expect.arrayContaining(['S', 't', 'r', 'i', 'n', 'g', 'M', 'y', '-']));
+
+    expect(actual.extraInfo).not.toBe(undefined);
+    expect(actual.extraInfo).not.toBeUndefined();
+    expect(actual.extraInfo).toBeDefined();
+    expect(actual.extraInfo).toBeTruthy();
   });
 });

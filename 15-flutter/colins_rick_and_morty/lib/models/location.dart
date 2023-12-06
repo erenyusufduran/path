44a31d28667fd6a95ed6083445faf68085383734
erenyusufduran <eren.duran@colins.com>
2023-12-06 16:56:54 +0300

    location screens

---
+++ 15-flutter/colins_rick_and_morty/lib/models/location.dart
@@ -0,0 +1,18 @@
+class Location {
+  int id;
+  String name;
+  String type;
+  String dimension;
+  List<String> residents;
+  String url;
+  String created;
+
+  Location(
+      {required this.id,
+      required this.name,
+      required this.type,
+      required this.dimension,
+      required this.residents,
+      required this.url,
+      required this.created});
+}

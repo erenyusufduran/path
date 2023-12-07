763e1c91d1d4cc9b9277920e933dda6e1d46b697
erenyusufduran <eren.duran@colins.com>
2023-12-07 17:21:19 +0300

    episode details add

--- 15-flutter/colins_rick_and_morty/lib/widgets/episode_line.dart
+++ 15-flutter/colins_rick_and_morty/lib/widgets/episode_line.dart
@@ -9,13 +9,20 @@ class EpisodeLine extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(children: [
       ListTile(
+          onTap: () {
+            Navigator.of(context)
+                .pushNamed("/episode_detail", arguments: episode);
+          },
           title: Text(episode.episode,
               style: TextStyle(fontWeight: FontWeight.w700)),
           subtitle: Text(episode.airDate),
           trailing: CircleAvatar(
               child: Text("+${episode.characters.length.toString()}")),
           leading: Text(episode.id.toString())),
-      Divider(color: Colors.black, height: 4,)
+      Divider(
+        color: Colors.black,
+        height: 4,
+      )
     ]);
   }
 }

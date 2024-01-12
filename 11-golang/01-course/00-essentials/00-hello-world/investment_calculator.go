3a5839a9b0213014e639aafc4163218dfee938ba
erenyusufduran <eren.duran@colins.com>
2024-01-12 18:13:25 +0300

    take from user

--- 11-golang/01-course/00-essentials/00-hello-world/investment_calculator.go
+++ 11-golang/01-course/00-essentials/00-hello-world/investment_calculator.go
@@ -6,10 +6,17 @@ import (
 )
 
 func main() {
-	const inflationRate = 2.5
-	var investmentAmount float64 = 1000
-	years := 10.0
-	expectedReturnRate := 5.5
+	const inflationRate = 6.5
+	var investmentAmount, years, expectedReturnRate float64
+
+	fmt.Print("Investment Amount: ")
+	fmt.Scan(&investmentAmount)
+
+	fmt.Print("Years: ")
+	fmt.Scan(&years)
+
+	fmt.Print("Expected Return Rate: ")
+	fmt.Scan(&expectedReturnRate)
 
 	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
 	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)

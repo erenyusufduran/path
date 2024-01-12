0f5d2091039b93f41db1081e75012c5dea11b395
erenyusufduran <eren.duran@colins.com>
2024-01-12 17:59:02 +0300

    constants

--- 11-golang/01-course/00-essentials/00-hello-world/investment_calculator.go
+++ 11-golang/01-course/00-essentials/00-hello-world/investment_calculator.go
@@ -6,9 +6,14 @@ import (
 )
 
 func main() {
-	var investmentAmount, years float64 = 1000, 10
+	const inflationRate = 2.5
+	var investmentAmount float64 = 1000
+	years := 10.0
 	expectedReturnRate := 5.5
 
 	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
+
 	fmt.Println(futureValue)
+	fmt.Println(futureRealValue)
 }

78b39067a43a6f4c14f60dc326187c513836331e
erenyusufduran <eren.duran@colins.com>
2024-01-15 00:46:57 +0300

    defining and instantiating struct, struct literal

--- 11-golang/01-course/00-essentials/04-structs/structs.go
+++ 11-golang/01-course/00-essentials/04-structs/structs.go
@@ -2,12 +2,27 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
+
+type user struct {
+	firstName string
+	lastName  string
+	birthDate string
+	createdAt time.Time
+}
 
 func main() {
 	firstName := getUserData("Please enter your first name: ")
 	lastName := getUserData("Please enter your last name: ")
 	birthDate := getUserData("Please enter your birthdate (MM/DD/YYYY): ")
+
+	appUser := user{
+		firstName: firstName,
+		lastName:  lastName,
+		birthDate: birthDate,
+		createdAt: time.Now(),
+	}
 
 	fmt.Println(firstName, lastName, birthDate)
 }

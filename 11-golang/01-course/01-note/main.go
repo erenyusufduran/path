ae1e70254e3112cf94e8655b7368e67a0735a686
erenyusufduran <eren.duran@colins.com>
2024-01-17 16:33:50 +0300

    getting user input and create struct

---
+++ 11-golang/01-course/01-note/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"example.com/note/note"
+	"fmt"
+)
+
+func main() {
+	title, content := getNoteData()
+	userNote, err := note.New(title, content)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+}
+
+func getNoteData() (string, string) {
+	title := getUserInput("Note title:")
+	content := getUserInput("Note content:")
+
+	return title, content
+}
+
+func getUserInput(prompt string) string {
+	fmt.Print(prompt)
+	var value string
+	fmt.Scanln(&value)
+	return value
+}

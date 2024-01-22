c5e9109edd8045ad60740c636596738e8fa1aa84
erenyusufduran <eren.duran@colins.com>
2024-01-22 14:48:30 +0300

    storing json data to files

--- 11-golang/01-course/02-price-calculator/prices/prices.go
+++ 11-golang/01-course/02-price-calculator/prices/prices.go
@@ -10,7 +10,7 @@ import (
 type TaxIncludedPriceJob struct {
 	TaxRate           float64
 	InputPrices       []float64
-	TaxIncludedPrices map[string]float64
+	TaxIncludedPrices map[string]string
 }
 
 func (job *TaxIncludedPriceJob) LoadData() {
@@ -38,7 +38,8 @@ func (job *TaxIncludedPriceJob) Process() {
 		result[fmt.Sprintf("%.2f", price)] = fmt.Sprintf("%.2f", taxIncludedPrice)
 	}
 
-	fmt.Println(result)
+	job.TaxIncludedPrices = result
+	filemanager.WriteJSON(fmt.Sprintf("result_%v.json", job.TaxRate), job)
 }
 
 func NewTaxIncludedPriceJob(taxRate float64) *TaxIncludedPriceJob {

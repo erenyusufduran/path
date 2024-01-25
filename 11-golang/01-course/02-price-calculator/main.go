09f5deb1191dff24a545064783c6c93801b97c4a
erenyusufduran <eren.duran@colins.com>
2024-01-25 12:40:30 +0300

    error handling with wrong way

--- 11-golang/01-course/02-price-calculator/main.go
+++ 11-golang/01-course/02-price-calculator/main.go
@@ -10,14 +10,17 @@ import (
 func main() {
 	taxRates := []float64{0, 0.7, 0.1, 0.12, 0.15}
 	doneChans := make([]chan bool, len(taxRates))
+	errorChans := make([]chan bool, len(taxRates))
 
 	for index, taxRate := range taxRates {
 		doneChans[index] = make(chan bool)
+		errorChans[index] = make(chan bool)
+
 		fm := filemanager.New("prices.txt", fmt.Sprintf("result_%v.json", taxRate*100))
 		// cmdm := cmdmanager.New()
 
 		priceJob := prices.NewTaxIncludedPriceJob(fm, taxRate)
-		go priceJob.Process(doneChans[index])
+		go priceJob.Process(doneChans[index], errorChans[index])
 		// if err != nil {
 		// 	fmt.Println("could not process job")
 		// 	fmt.Println(err)

58e8d2ef5498384ca40a715dee4834581026a1c3
Eren Yusuf Duran <eren.duran@colins.com>
2024-02-21 19:03:43 +0300

    structure of the dining philosophers problem

---
+++ 11-golang/02-course-by-trevor-sawler/concurrency/dining-philosophers/main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+// The Dining Philosophers problem is well known in computer science circles.
+// Five philosophers, numbered from 0 through 4, live in a house where the
+// table is laid for them; each philosopher has their own place at the table.
+// Their only difficulty - besides those of philosophy - is that the dish
+// server is a very difficult kind of spaghetti which has to be eaten with
+// two forks. There are two forks next to each plate, so that presents no
+// difficulty. As a consequence, however, this means that no two neighbours
+// may be eating simultaneously, since there are five philophers and five forks.
+//
+// This is a simple implementation of Dijkstra's solution to the
+// "Dining Philosophers" dilemma.
+
+// Philosopher is a struct which stores information about a philosopher.
+type Philosopher struct {
+	name      string
+	rightFork int
+	leftFork  int
+}
+
+// list of all philosophers
+var philosophers = []Philosopher{
+	{name: "Plato", leftFork: 4, rightFork: 0},
+	{name: "Socrates", leftFork: 0, rightFork: 1},
+	{name: "Aristotle", leftFork: 1, rightFork: 2},
+	{name: "Pascal", leftFork: 2, rightFork: 3},
+	{name: "Locke", leftFork: 3, rightFork: 4},
+}
+
+// some variables
+var hunger = 3 // how many times does a person eat?
+var eatTime = 1 * time.Second
+var thinkTime = 3 * time.Second
+var sleepTime = 1 * time.Second
+
+func main() {
+	fmt.Println("Dining Philosophers Problem")
+	fmt.Println("---------------------------")
+	fmt.Println("The table is empty.")
+
+	// start the meal
+	dine()
+
+	fmt.Println("The table is empty.")
+}
+
+func dine() {
+	wg := &sync.WaitGroup{}
+	wg.Add(len(philosophers))
+
+	seated := &sync.WaitGroup{}
+	seated.Add(len(philosophers))
+
+	// forks is a map of all 5 forks.
+	var forks = make(map[int]*sync.Mutex)
+	for i := 0; i < len(philosophers); i++ {
+		forks[i] = &sync.Mutex{}
+	}
+
+	// start the meal
+	for i := 0; i < len(philosophers); i++ {
+		// fire off a goroutine for the current philosopher
+		go diningProblem(philosophers[i], wg, forks, seated)
+	}
+
+	wg.Wait()
+}
+
+func diningProblem(philosopher Philosopher, wg *sync.WaitGroup, forks map[int]*sync.Mutex, seated *sync.WaitGroup) {
+	defer wg.Done()
+}

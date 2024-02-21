b85b37a6334d6e2b1b7b7db57481e07555a2443b
Eren Yusuf Duran <eren.duran@colins.com>
2024-02-21 18:03:50 +0300

    finishing producer code

--- 11-golang/02-course-by-trevor-sawler/concurrency/producer-consumer/main.go
+++ 11-golang/02-course-by-trevor-sawler/concurrency/producer-consumer/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"math/rand"
 	"time"
 
@@ -11,32 +12,103 @@ const NumberOfPizzas = 10
 
 var pizzasMade, pizzasFailed, total int
 
+// Producer is a type for structs that holds two channels: one for pizzas,
+// with all information for a given pizza order including whether it was made
+// successfully, Another to handle end of processing (when we quit the channel)
 type Producer struct {
 	data chan PizzaOrder
 	quit chan chan error
 }
 
+// PizzaOrder is a type for structs that describes a given pizza order.
+// It has the order number, a message indicating what happened to the order,
+// and a boolean indicating if the order was successfully completed.
 type PizzaOrder struct {
 	pizzaNumber int
 	message     string
 	success     bool
 }
 
+// Close is simply a method of closing the channel when we are done
+// with it. For example If something is pushed to the quit channel
 func (p *Producer) Close() error {
 	ch := make(chan error)
 	p.quit <- ch
 	return <-ch
 }
 
+// makePizza attemps to make a pizza. We generate a random number from 1-12,
+// and put in two cases where we can't make the pizza in time. Otherwise,
+// we make the pizza without issue. To make things interesting, each pizza
+// will take a different length of time to produce (some pizzas are harder than others.)
+func makePizza(pizzaNumber int) *PizzaOrder {
+	pizzaNumber++
+	if pizzaNumber <= NumberOfPizzas {
+		delay := rand.Intn(5) + 1
+		fmt.Printf("Received order #%d!\n", pizzaNumber)
+
+		rnd := rand.Intn(12) + 1
+		msg := ""
+		success := false
+
+		if rnd < 5 {
+			pizzasFailed++
+		} else {
+			pizzasMade++
+		}
+		total++
+
+		fmt.Printf("Making pizza #%d. It will take %d seconds..\n", pizzaNumber, delay)
+		// delay for a bit
+		time.Sleep(time.Duration(delay) * time.Second)
+
+		if rnd <= 2 {
+			msg = fmt.Sprintf("*** We ran out of ingredients for pizza #%d!", pizzaNumber)
+		} else if rnd <= 4 {
+			msg = fmt.Sprintf("*** the cook quit while making pizza #%d!", pizzaNumber)
+		} else {
+			success = true
+			msg = fmt.Sprintf("Pizza order #%d is ready!", pizzaNumber)
+		}
+
+		p := PizzaOrder{
+			pizzaNumber: pizzaNumber,
+			message:     msg,
+			success:     success,
+		}
+		return &p
+	}
+	return &PizzaOrder{
+		pizzaNumber: pizzaNumber,
+	}
+}
+
+// pizzeria is a goroutine that runs in the background and
+// cals makePizza to try to make one order each time it iterates through
+// the for loop. It executes until it receives something on the quit channel.
+// The quit channel does not reecive anything until the consumer
+// sends it (when the number of orders is greater than or equest
+// to the constant NumberOfPizzas).
 func pizzeria(pizzaMaker *Producer) {
 	// keep track of which pizza we are making
+	var i = 0
 
 	// run forever or until we receive a quit notification
 	// try to make pizzas
 	for {
-		// try to make a pizza
-
-		// decision
+		currentPizza := makePizza(i)
+		if currentPizza != nil {
+			i = currentPizza.pizzaNumber
+			select {
+			// we tried to make a pizza (we sent something to the data channel)
+			case pizzaMaker.data <- *currentPizza:
+			case quitChan := <-pizzaMaker.quit:
+				// close channels
+				close(pizzaMaker.data)
+				close(quitChan)
+				return
+			}
+		}
 	}
 }
 

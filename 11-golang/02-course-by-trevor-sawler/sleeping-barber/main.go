98a2a1a61c490fbc4935903b71f00407f609b6c7
Eren Duran <eren.duran@colins.com>
2024-02-25 16:54:43 +0300

    getting start to barbershop coding

--- 11-golang/02-course-by-trevor-sawler/sleeping-barber/main.go
+++ 11-golang/02-course-by-trevor-sawler/sleeping-barber/main.go
@@ -1,13 +1,41 @@
 package main
 
+import (
+	"math/rand"
+	"time"
+
+	"github.com/fatih/color"
+)
+
+// package level variables
+var seatingCapacity = 10
+var arrivalRate = 100
+var cutDuration = 1000 * time.Millisecond
+var timeOpen = 10 * time.Second
+
 func main() {
 	// seed our random number generator
+	rand.New(rand.NewSource(time.Now().UnixNano()))
 
 	// print welcome message
+	color.Yellow("The Sleeping barber Problem")
+	color.Yellow("---------------------------")
 
 	// create channels if we need any
+	clientChan := make(chan string, seatingCapacity) // maximum ten people in this channel
+	doneChan := make(chan bool)                      // when everythings done.
 
 	// create the barbershop
+	shop := BarberShop{
+		ShopCapacity:    seatingCapacity,
+		HairCutDuration: cutDuration,
+		NumberOfBarbers: 0,
+		ClientsChan:     clientChan,
+		BarbersDoneChan: doneChan,
+		Open:            true,
+	}
+
+	color.Green("The shop is open for the day!")
 
 	// add barbers
 

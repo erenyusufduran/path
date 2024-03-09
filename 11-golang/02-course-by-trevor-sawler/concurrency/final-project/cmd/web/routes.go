4b47551300212d5331db764e481b4cc9287d83bb
Eren Duran <eren.duran@colins.com>
2024-03-09 16:24:22 +0300

    adding route and plan page

--- 11-golang/02-course-by-trevor-sawler/concurrency/final-project/cmd/web/routes.go
+++ 11-golang/02-course-by-trevor-sawler/concurrency/final-project/cmd/web/routes.go
@@ -24,5 +24,6 @@ func (app *Config) routes() http.Handler {
 	mux.Post("/register", app.PostRegisterPage)
 	mux.Get("/activate", app.ActivateAccount)
 
+	mux.Get("/plans", app.ChooseSubscription)
 	return mux
 }

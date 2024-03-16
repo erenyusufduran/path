cc6e322383202a13db13af3084d44ddcc1722a35
Eren Duran <eren.duran@colins.com>
2024-03-16 07:18:33 +0300

    mongo added to docker-compose and auth service log added

--- 11-golang/02-course-by-trevor-sawler/go-micro/logger-service/cmd/api/handlers.go
+++ 11-golang/02-course-by-trevor-sawler/go-micro/logger-service/cmd/api/handlers.go
@@ -22,14 +22,18 @@ type JSONPayload struct {
 
 func (app *Config) WriteLog(w http.ResponseWriter, r *http.Request) {
 	var requestPayload JSONPayload
-	_ = t.ReadJSON(w, r, &requestPayload)
+	err := t.ReadJSON(w, r, &requestPayload)
+	if err != nil {
+		t.ErrorJSON(w, err)
+		return
+	}
 
 	event := data.LogEntry{
 		Name: requestPayload.Name,
 		Data: requestPayload.Data,
 	}
 
-	err := app.Models.LogEntry.Insert(event)
+	err = app.Models.LogEntry.Insert(event)
 	if err != nil {
 		t.ErrorJSON(w, err)
 		return

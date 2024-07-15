0e263aa8724a759b6dc16058bceb8ac9802461c3
Eren Yusuf Duran <eren.duran@colins.com>
2024-07-15 16:03:34 +0300

    controller decorators

--- 01-nodejs/nest/00-fundamentals/iluvcoffee/src/coffees/coffees.controller.ts
+++ 01-nodejs/nest/00-fundamentals/iluvcoffee/src/coffees/coffees.controller.ts
@@ -1,19 +1,27 @@
 import {
   Body,
   Controller,
+  Delete,
   Get,
   HttpCode,
   HttpStatus,
   Param,
+  Patch,
   Post,
+  Query,
   Res,
 } from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
   @Get()
-  findAll(@Res() response) {
-    response.status(200).send('This action returns all coffees');
+  findAll(@Query() paginationQuery, @Res() response) {
+    const { limit, offset } = paginationQuery;
+    return response
+      .status(200)
+      .send(
+        `This action returns all coffees. Limit: ${limit}, Offset: ${offset}`,
+      );
   }
 
   @Get(':id')
@@ -25,5 +33,15 @@ export class CoffeesController {
   @HttpCode(HttpStatus.GONE)
   create(@Body() body) {
     return body;
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() body) {
+    return `This action updates #${id} coffee`;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `This action removes #${id} coffee`;
   }
 }
